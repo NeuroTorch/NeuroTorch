e43618fb27e13c324e26297570a4615196313453
Jérémie Gince <50332514+JeremieGince@users.noreply.github.com>
2022-05-16 21:20:29 -0400

    Update sequential.py

--- src/neurotorch/modules/sequential.py
+++ src/neurotorch/modules/sequential.py
@@ -15,7 +15,7 @@ import torch.nn.functional as F
 from . import (
 	BaseModel,
 	HeavisideSigmoidApprox,
-	RNNLayer,
+	BaseLayer,
 	LIFLayer,
 	LayerType,
 	LayerType2Layer,
@@ -24,13 +24,28 @@ from . import (
 	SpikeFuncType2Func,
 	SpikeFunction
 )
+from ..dimension import Dimension
 
 Acceptable_Spike_Func = Union[Type[SpikeFunction], SpikeFuncType]
-Acceptable_Layer_Type = Union[Type[RNNLayer], LayerType]
+Acceptable_Spike_Funcs = Union[Acceptable_Spike_Func, Iterable[Acceptable_Spike_Func]]
+Acceptable_Layer_Type = Union[Type[BaseLayer], LayerType]
+Acceptable_Layer_Types = Union[Acceptable_Layer_Type, Iterable[Acceptable_Layer_Type]]
+IntDimension = Union[int, Dimension]
 
 
 class SequentialModel(BaseModel):
-	def __new__(cls, *args, **kwargs):
+	def __new__(
+			cls,
+			*,
+			input_sizes: Optional[Union[Dict[str, IntDimension], List[int], IntDimension]] = None,
+			output_sizes: Optional[Union[Dict[str, int], List[int], int]] = None,
+			n_hidden_neurons: Optional[Union[int, Iterable[int]]] = None,
+			use_recurrent_connection: Optional[Union[bool, Iterable[bool]]] = None,
+			spike_funcs: Optional[Acceptable_Spike_Funcs] = None,
+			hidden_layer_types: Optional[Acceptable_Layer_Types] = None,
+			readout_layer_type: Optional[Acceptable_Layer_Type] = None,
+			layers: Optional[Iterable[BaseLayer]] = None,
+			**kwargs):
 		# TODO: if the first argument is a iterable of layer, juste call the constructor
 		# TODO: instead create the iterable of layers with the named parameters and call the constructor
 		# TODO: the call of the constructor should be like this:
@@ -40,18 +55,24 @@ class SequentialModel(BaseModel):
 		#   output_layers=...
 		# )
 		# exemple: https://stackoverflow.com/questions/682504/what-is-a-clean-pythonic-way-to-implement-multiple-constructors
-		raise NotImplementedError
+		auto_construct_parameters = [
+				input_sizes,
+				output_sizes,
+				n_hidden_neurons,
+				use_recurrent_connection,
+				spike_funcs,
+				hidden_layer_types,
+				readout_layer_type
+			]
+		if layers is not None:
+			assert all([p is None for p in auto_construct_parameters]), \
+				f"You can't use the named parameters: {auto_construct_parameters} and layers at the same time"
+			return cls(layers=layers, **kwargs)
+		raise NotImplementedError()
 	
 	def __init__(
 			self,
-			# input_sizes: Union[Dict[str, int], List[int], int],
-			# output_sizes: Union[Dict[str, int], List[int], int],
-			# n_hidden_neurons: Optional[Union[int, Iterable[int]]] = None,
-			# use_recurrent_connection: Union[bool, Iterable[bool]] = True,
-			# spike_funcs: Union[Acceptable_Spike_Func, Iterable[Acceptable_Spike_Func]] = HeavisideSigmoidApprox,
-			# hidden_layer_types: Union[Acceptable_Layer_Type, Iterable[Acceptable_Layer_Type]] = LIFLayer,
-			# readout_layer_type: Optional[Acceptable_Layer_Type] = LILayer,
-			layers: Iterable[RNNLayer],  # TODO: change the inputs to be more flexible
+			layers: Iterable[Union[Iterable[BaseLayer], BaseLayer]],
 			int_time_steps: int = 100,
 			name: str = "snn",
 			checkpoint_folder: str = "checkpoints",
@@ -59,27 +80,56 @@ class SequentialModel(BaseModel):
 			input_transform: Optional[Union[Dict[str, Callable], List[Callable]]] = None,
 			**kwargs
 	):
+		self.input_layers, self.hidden_layers, self.output_layers = self._format_layers_(layers)
 		super(SequentialModel, self).__init__(
-			input_sizes=input_sizes,
-			output_size=output_sizes,
+			input_sizes={layer.name: layer.input_size for layer in self.input_layers},
+			output_size={layer.name: layer.output_size for layer in self.output_layers},
 			name=name,
 			checkpoint_folder=checkpoint_folder,
 			device=device,
 			input_transform=input_transform,
 			**kwargs
 		)
-		self.dt = self.kwargs.pop("dt", 1e-3)
 		self.int_time_steps = int_time_steps
-		self.n_hidden_neurons = self._format_hidden_neurons_(n_hidden_neurons)
-		self.spike_func = self._format_spike_funcs_(spike_funcs)
-		self.hidden_layer_types: List[Type] = self._format_layer_types_(hidden_layer_types)
-		self.readout_layer_type = self._format_layer_type_(readout_layer_type)  # TODO: change for multiple readout layers
-		self.use_recurrent_connection = use_recurrent_connection
-		self.input_layers = nn.ModuleDict()
-		self.hidden_layers = nn.ModuleDict()
-		self.output_layers = nn.ModuleDict()
-		self._add_layers_()
+		# self.n_hidden_neurons = self._format_hidden_neurons_(n_hidden_neurons)
+		# self.spike_func = self._format_spike_funcs_(spike_funcs)
+		# self.hidden_layer_types: List[Type] = self._format_layer_types_(hidden_layer_types)
+		# self.readout_layer_type = self._format_layer_type_(readout_layer_type)  # TODO: change for multiple readout layers
+		# self._add_layers_()
 		self.initialize_weights_()
+		
+	@staticmethod
+	def _format_input_output_layers(layers: Iterable[Union[Iterable[BaseLayer], BaseLayer]]) -> nn.ModuleDict:
+		layers: Iterable[BaseLayer] = [layers] if not isinstance(layers, Iterable) else layers
+		assert all([isinstance(layer, BaseLayer) for layer in layers]), \
+			"All layers must be of type BaseLayer"
+		if not isinstance(layers, dict):
+			layers = {layer.name: layer for layer in layers}
+		return nn.ModuleDict({layer.name: layer for layer in layers})
+	
+	@staticmethod
+	def _format_layers(
+			layers: Iterable[Union[Iterable[BaseLayer], BaseLayer]]
+	) -> Tuple[nn.ModuleDict, nn.ModuleList, nn.ModuleDict]:
+		if not isinstance(layers, Iterable):
+			layers = [layers]
+		if len(layers) > 1:
+			input_layers = SequentialModel._format_input_output_layers(layers[0])
+		else:
+			input_layers = nn.ModuleDict()
+		
+		if len(layers) > 2:
+			hidden_layers = layers[1:-1]
+			if not isinstance(hidden_layers, Iterable):
+				hidden_layers = [hidden_layers]
+			assert all([isinstance(layer, BaseLayer) for layer in hidden_layers]), \
+				"All hidden layers must be of type BaseLayer"
+		else:
+			hidden_layers = []
+		
+		hidden_layers = nn.ModuleList(hidden_layers)
+		output_layers = SequentialModel._format_input_output_layers(layers[-1])
+		return input_layers, hidden_layers, output_layers
 
 	def _format_spike_funcs_(
 			self,
@@ -97,7 +147,7 @@ class SequentialModel(BaseModel):
 	def _format_layer_types_(
 			self,
 			layer_types: Union[Acceptable_Layer_Type, Iterable[Acceptable_Layer_Type]]
-	) -> List[Type[RNNLayer]]:
+	) -> List[Type[BaseLayer]]:
 		if not isinstance(layer_types, Iterable):
 			layer_types = [layer_types]
 		for i, layer_type in enumerate(layer_types):
@@ -109,7 +159,7 @@ class SequentialModel(BaseModel):
 	@staticmethod
 	def _format_layer_type_(
 			layer_type: Optional[Acceptable_Layer_Type]
-	) -> Optional[Type[RNNLayer]]:
+	) -> Optional[Type[BaseLayer]]:
 		if isinstance(layer_type, LayerType):
 			layer_type = LayerType2Layer[layer_type]
 		return layer_type
@@ -145,7 +195,6 @@ class SequentialModel(BaseModel):
 				input_size=hn,
 				output_size=n_hidden_neurons[i + 1],
 				use_recurrent_connection=self.use_recurrent_connection,
-				dt=self.dt,
 				spike_func=self.spike_funcs[i + 1],
 				device=self.device,
 				**self.kwargs
@@ -292,151 +341,6 @@ class SequentialModel(BaseModel):
 			if isinstance(self.hidden_layers[l_name], LIFLayer):
 				counts.extend(traces[-1].sum(dim=(0, 1)).tolist())
 		return torch.tensor(counts, dtype=torch.float32, device=self.device)
-
-	def _check_early_stopping(self, patience: int, tol: float = 1e-2) -> bool:
-		"""
-		:param patience:
-		:return:
-		"""
-		losses = self.loss_history['val'][-patience:]
-		return np.all(np.abs(np.diff(losses)) < tol)
-
-	def fit(
-			self,
-			train_dataloader: DataLoader,
-			val_dataloader: DataLoader,
-			lr=1e-3,
-			nb_epochs=15,
-			criterion=None,
-			optimizer=None,
-			load_checkpoint_mode: LoadCheckpointMode = None,
-			force_overwrite: bool = False,
-			early_stopping: bool = False,
-			early_stopping_patience: int = 5,
-			verbose: bool = True,
-			p_bar_position: Optional[int] = None,
-			p_bar_leave: Optional[bool] = None,
-	):
-		if criterion is None:
-			criterion = nn.NLLLoss()
-		if optimizer is None:
-			optimizer = torch.optim.Adam(self.parameters(), lr=lr, weight_decay=1e-5)
-
-		start_epoch = 0
-		if load_checkpoint_mode is None:
-			assert os.path.exists(self.checkpoints_meta_path) or force_overwrite, \
-				f"{self.checkpoints_meta_path} already exists. " \
-				f"Set force_overwrite flag to True to overwrite existing saves."
-			if os.path.exists(self.checkpoints_meta_path) and force_overwrite:
-				shutil.rmtree(self.checkpoint_folder)
-		else:
-			try:
-				checkpoint = self.load_checkpoint(load_checkpoint_mode)
-				self.load_state_dict(checkpoint[SequentialModel.CHECKPOINT_STATE_DICT_KEY], strict=True)
-				optimizer.load_state_dict(checkpoint[SequentialModel.CHECKPOINT_OPTIMIZER_STATE_DICT_KEY])
-				start_epoch = int(checkpoint[SequentialModel.CHECKPOINT_EPOCH_KEY]) + 1
-				self.loss_history = self.get_checkpoints_loss_history()
-			except FileNotFoundError:
-				if verbose:
-					logging.warning("No such checkpoint. Fit from beginning.", UserWarning)
-
-		if start_epoch >= nb_epochs:
-			return self.loss_history
-
-		best_loss = self.loss_history.min('val')
-		p_bar = tqdm(
-			range(start_epoch, nb_epochs),
-			desc="Training",
-			disable=not verbose,
-			position=p_bar_position,
-			unit="epoch",
-			leave=p_bar_leave
-		)
-		for epoch in p_bar:
-			epoch_loss = self._exec_phase(train_dataloader, val_dataloader, criterion, optimizer)
-			epoch_val_acc = self.compute_classification_accuracy(val_dataloader, verbose=False)
-			self.loss_history.concat(epoch_loss)
-			is_best = epoch_loss['val'] < best_loss
-			self.save_checkpoint(optimizer, epoch, epoch_loss, is_best)
-			if is_best:
-				best_loss = epoch_loss['val']
-			p_bar.set_postfix(
-				train_loss=f"{epoch_loss['train']:.5e}",
-				val_loss=f"{epoch_loss['val']:.5e}",
-				val_acc=f"{epoch_val_acc:.5f}",
-			)
-			if early_stopping and self._check_early_stopping(early_stopping_patience):
-				if verbose:
-					logging.info(f"Early stopping stopped the training at epoch {epoch}.")
-				break
-		p_bar.close()
-		self.plot_loss_history(show=False)
-		return self.loss_history
-
-	def _exec_phase(self, train_dataloader, val_dataloader, criterion, optimizer):
-		self.train()
-		train_loss = self._exec_epoch(
-			train_dataloader,
-			criterion,
-			optimizer,
-		)
-		self.eval()
-		val_loss = self._exec_epoch(
-			val_dataloader,
-			criterion,
-			optimizer,
-		)
-		return dict(train=train_loss, val=val_loss)
-
-	def _exec_epoch(
-			self,
-			dataloader,
-			criterion,
-			optimizer,
-	):
-		batch_losses = []
-		for x_batch, y_batch in dataloader:
-			batch_loss = self._exec_batch(
-				x_batch,
-				y_batch,
-				criterion,
-				optimizer,
-			)
-			batch_losses.append(batch_loss)
-		return np.mean(batch_losses)
-
-	def _exec_batch(
-			self,
-			x_batch,
-			y_batch,
-			criterion,
-			optimizer,
-	):
-		if self.training:
-			log_p_y, out, h_sates = self.get_prediction_log_proba(
-				x_batch, re_outputs_trace=True, re_hidden_states=True
-			)
-		else:
-			with torch.no_grad():
-				log_p_y, out, h_sates = self.get_prediction_log_proba(
-					x_batch, re_outputs_trace=True, re_hidden_states=True
-				)
-
-		# TODO: add regularization loss
-		# reg_loss = torch.mean(self.get_spikes_count_per_neuron(h_sates))
-		# spikes = [h[-1] for l_name, h_list in h_sates.items() for h in h_list if l_name.lower() != "readout"]
-		# reg_loss = 1e-5 * sum([torch.sum(s) for s in spikes])  # L1 loss on total number of spikes
-		# reg_loss = 1e-5 * sum(
-		# 	[torch.mean(torch.sum(torch.sum(s, dim=0), dim=0) ** 2) for s in spikes]
-		# )  # L2 loss on spikes per neuron
-		# reg_loss = torch.mean(self.get_spikes_count_per_neuron(h_sates) ** 2)
-
-		batch_loss = criterion(log_p_y, y_batch.long().to(self.device))
-		if self.training:
-			optimizer.zero_grad()
-			batch_loss.backward()
-			optimizer.step()
-		return batch_loss.item()
 
 	def plot_loss_history(self, loss_history: LossHistory = None, show=False):
 		if loss_history is None:

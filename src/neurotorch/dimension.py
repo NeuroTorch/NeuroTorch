070b010f5187bd4a1b9936dbb6ec2ae89c151049
Jérémie Gince <50332514+JeremieGince@users.noreply.github.com>
2022-06-01 22:52:45 -0400

    Update dimension.py

--- src/neurotorch/dimension.py
+++ src/neurotorch/dimension.py
@@ -1,5 +1,5 @@
 import enum
-from typing import NamedTuple, Union
+from typing import NamedTuple, Union, Optional
 
 
 class DimensionProperty(enum.Enum):
@@ -7,18 +7,24 @@ class DimensionProperty(enum.Enum):
 	TIME = 1
 	SPATIAL = 2
 	
-	
+
 class Dimension(NamedTuple):
-	size: int
+	size: Optional[int]
 	dtype: DimensionProperty
 	
+	def __int__(self):
+		return self.size
+	
+	def __str__(self):
+		return f"{self.dtype.name}:{self.size}"
+	
 	@staticmethod
-	def from_int(size: int) -> "Dimension":
+	def from_int(size: Optional[int]) -> "Dimension":
 		return Dimension(size, DimensionProperty.NONE)
 	
 	@staticmethod
-	def from_int_or_dimension(dimension: Union[int, "Dimension"]) -> "Dimension":
-		if isinstance(dimension, int):
+	def from_int_or_dimension(dimension: Optional[Union[int, "Dimension"]]) -> "Dimension":
+		if isinstance(dimension, int) or dimension is None:
 			return Dimension.from_int(dimension)
 		return dimension
 

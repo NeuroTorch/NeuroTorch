44fcc83347dd8952b49febee8909b5a297416870
Jérémie Gince <50332514+JeremieGince@users.noreply.github.com>
2022-06-11 13:27:33 -0400

    Update main.py

--- applications/mnist/main.py
+++ applications/mnist/main.py
@@ -1,6 +1,7 @@
 import os
 import pprint
 from typing import Any, Dict
+from collections import OrderedDict
 
 import psutil
 
@@ -17,6 +18,7 @@ def train_with_params(params: Dict[str, Any], n_iterations: int = 100, data_fold
 	checkpoints_name = str(hash_params(params))
 	checkpoint_folder = f"{data_folder}/{checkpoints_name}"
 	os.makedirs(checkpoint_folder, exist_ok=True)
+	print(f"Checkpoint folder: {checkpoint_folder}")
 
 	dataloaders = get_dataloaders(
 		dataset_id=params["dataset_id"],
@@ -61,27 +63,42 @@ def train_with_params(params: Dict[str, Any], n_iterations: int = 100, data_fold
 		# force_overwrite=True,
 		verbose=verbose,
 	)
-	network.load_checkpoint(checkpoint_manager.checkpoints_meta_path, LoadCheckpointMode.BEST_ITR)
-	return dict(
+	try:
+		network.load_checkpoint(checkpoint_manager.checkpoints_meta_path, LoadCheckpointMode.BEST_ITR, verbose=verbose)
+	except FileNotFoundError:
+		network.load_checkpoint(checkpoint_manager.checkpoints_meta_path, LoadCheckpointMode.LAST_ITR, verbose=verbose)
+	return OrderedDict(dict(
 		network=network,
+		checkpoints_name=checkpoints_name,
 		accuracies={
-			k: ClassificationMetrics.accuracy(network, dataloaders[k], verbose=True, desc=k)
+			k: ClassificationMetrics.accuracy(network, dataloaders[k], verbose=True, desc=f"{k}_accuracy")
 			for k in dataloaders
 		},
-		checkpoints_name=checkpoints_name,
-	)
+		precisions={
+			k: ClassificationMetrics.precision(network, dataloaders[k], verbose=True, desc=f"{k}_precision")
+			for k in dataloaders
+		},
+		recalls={
+			k: ClassificationMetrics.recall(network, dataloaders[k], verbose=True, desc=f"{k}_recall")
+			for k in dataloaders
+		},
+		f1s={
+			k: ClassificationMetrics.f1(network, dataloaders[k], verbose=True, desc=f"{k}_f1")
+			for k in dataloaders
+		},
+	))
 
 
 if __name__ == '__main__':
 	results = train_with_params(
 		{
 			"dataset_id": DatasetId.MNIST,
-			"to_spikes_use_periods": True,
+			"to_spikes_use_periods": False,
 			"use_recurrent_connection": False,
 			"n_hidden_layers": 0,
 			"n_hidden_neurons": 128,
 			"learn_beta": False,
-			"n_steps": 100,
+			"n_steps": 2,
 			"train_val_split_ratio": 0.95,
 		},
 		n_iterations=100,

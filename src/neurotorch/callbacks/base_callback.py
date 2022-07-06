7a2dc82124686d7aceb0a430ed24c74e71432460
Jérémie Gince <50332514+JeremieGince@users.noreply.github.com>
2022-07-06 16:29:23 -0400

    add doc

--- src/neurotorch/callbacks/base_callback.py
+++ src/neurotorch/callbacks/base_callback.py
@@ -3,36 +3,107 @@ from typing import List, Iterable, Optional
 
 class BaseCallback:
 	def start(self, trainer):
+		"""
+		Called when the training starts. This is the first callback called.
+		:param trainer: The trainer.
+		:return: None
+		"""
 		pass
 	
 	def close(self, trainer):
+		"""
+		Called when the training ends. This is the last callback called.
+		:param trainer: The trainer.
+		:return: None
+		"""
 		pass
-	
+
 	def on_train_begin(self, trainer):
+		"""
+		Called when the train phase of an iteration starts. The train phase is defined as a full pass through the
+		training dataset.
+		:param trainer: The trainer.
+		:return: None
+		"""
 		pass
-	
+
 	def on_train_end(self, trainer):
+		"""
+		Called when the train phase of an iteration ends. The train phase is defined as a full pass through the
+		training dataset.
+		:param trainer: The trainer.
+		:return: None
+		"""
 		pass
-	
+
 	def on_epoch_begin(self, trainer):
+		"""
+		Called when an epoch starts. An epoch is defined as one full pass through the training dataset or
+		the validation dataset.
+		:param trainer: The trainer.
+		:return: None
+		"""
 		pass
-	
+
 	def on_epoch_end(self, trainer):
+		"""
+		Called when an epoch ends. An epoch is defined as one full pass through the training dataset or
+		the validation dataset.
+		:param trainer: The trainer.
+		:return: None
+		"""
 		pass
-	
+
 	def on_batch_begin(self, trainer):
+		"""
+		Called when a batch starts. The batch is defined as one forward pass through the network.
+		:param trainer: The trainer.
+		:return: None
+		"""
 		pass
-	
+
 	def on_batch_end(self, trainer):
+		"""
+		Called when a batch ends. The batch is defined as one forward pass through the network.
+		:param trainer: The trainer.
+		:return: None
+		"""
 		pass
-	
+
 	def on_validation_begin(self, trainer):
+		"""
+		Called when the validation phase of an iteration starts. The validation phase is defined as a full pass through
+		the validation dataset.
+		:param trainer:
+		:return:
+		"""
 		pass
-	
+
 	def on_validation_end(self, trainer):
+		"""
+		Called when the validation phase of an iteration ends. The validation phase is defined as a full pass through
+		the validation dataset.
+		:param trainer: The trainer.
+		:return: None
+		"""
+		pass
+
+	def on_iteration_begin(self, trainer):
+		"""
+		Called when an iteration starts. An iteration is defined as one full pass through the training dataset and
+		the validation dataset.
+		:param trainer: The trainer.
+		:return: None
+		"""
 		pass
 
 	def on_iteration_end(self, trainer):
+		"""
+		Called when an iteration ends. An iteration is defined as one full pass through the training dataset and
+		the validation dataset.
+		:param trainer: The trainer.
+		:return: None
+		"""
 		pass
 
 	
@@ -53,46 +124,118 @@ class CallbacksList:
 		return self._length
 	
 	def start(self, trainer):
+		"""
+		Called when the trainer starts.
+		:param trainer: The trainer.
+		:return: None
+		"""
 		for callback in self.callbacks:
 			callback.start(trainer)
 	
 	def close(self, trainer):
+		"""
+		Called when the trainer closes.
+		:param trainer: The trainer.
+		:return: None
+		"""
 		for callback in self.callbacks:
 			callback.close(trainer)
 	
 	def on_train_begin(self, trainer):
+		"""
+		Called when the train phase of an iteration starts. The train phase is defined as a full pass through the
+		training dataset.
+		:param trainer: The trainer.
+		:return: None
+		"""
 		for callback in self.callbacks:
 			callback.on_train_begin(trainer)
 	
 	def on_train_end(self, trainer):
+		"""
+		Called when the train phase of an iteration ends. The train phase is defined as a full pass through the
+		training dataset.
+		:param trainer: The trainer.
+		:return: None
+		"""
 		for callback in self.callbacks:
 			callback.on_train_end(trainer)
 	
 	def on_epoch_begin(self, trainer):
+		"""
+		Called when an epoch starts. An epoch is defined as one full pass through the training dataset or
+		the validation dataset.
+		:param trainer: The trainer.
+		:return: None
+		"""
 		for callback in self.callbacks:
 			callback.on_epoch_begin(trainer)
 	
 	def on_epoch_end(self, trainer):
+		"""
+		Called when an epoch ends. An epoch is defined as one full pass through the training dataset or
+		the validation dataset.
+		:param trainer: The trainer.
+		:return: None
+		"""
 		for callback in self.callbacks:
 			callback.on_epoch_end(trainer)
 	
 	def on_batch_begin(self, trainer):
+		"""
+		Called when a batch starts. The batch is defined as one forward pass through the network.
+		:param trainer: The trainer.
+		:return: None
+		"""
 		for callback in self.callbacks:
 			callback.on_batch_begin(trainer)
 	
 	def on_batch_end(self, trainer):
+		"""
+		Called when a batch ends. The batch is defined as one forward pass through the network.
+		:param trainer: The trainer.
+		:return: None
+		"""
 		for callback in self.callbacks:
 			callback.on_batch_end(trainer)
 	
 	def on_validation_begin(self, trainer):
+		"""
+		Called when the validation phase of an iteration starts. The validation phase is defined as a full pass through
+		the validation dataset.
+		:param trainer:
+		:return:
+		"""
 		for callback in self.callbacks:
 			callback.on_validation_begin(trainer)
 	
 	def on_validation_end(self, trainer):
+		"""
+		Called when the validation phase of an iteration ends. The validation phase is defined as a full pass through
+		the validation dataset.
+		:param trainer: The trainer.
+		:return: None
+		"""
 		for callback in self.callbacks:
 			callback.on_validation_end(trainer)
 
+	def on_iteration_begin(self, trainer):
+		"""
+		Called when an iteration starts. An iteration is defined as one full pass through the training dataset and
+		the validation dataset.
+		:param trainer: The trainer.
+		:return: None
+		"""
+		for callback in self.callbacks:
+			callback.on_iteration_begin(trainer)
+
 	def on_iteration_end(self, trainer):
+		"""
+		Called when an iteration ends. An iteration is defined as one full pass through the training dataset and
+		the validation dataset.
+		:param trainer: The trainer.
+		:return: None
+		"""
 		for callback in self.callbacks:
 			callback.on_iteration_end(trainer)
 

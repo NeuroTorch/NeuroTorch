9604fc0262832dee3eb31326aad4f4951161babd
Jérémie Gince <50332514+JeremieGince@users.noreply.github.com>
2022-07-06 16:30:28 -0400

    Update history.py
    add methods to be able to update a already created figure

--- src/neurotorch/callbacks/history.py
+++ src/neurotorch/callbacks/history.py
@@ -1,5 +1,5 @@
 from collections import defaultdict
-from typing import Dict, List
+from typing import Dict, List, Tuple
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -26,7 +26,9 @@ class TrainingHistory(BaseCallback):
 		return iter(self.container)
 
 	def __len__(self):
-		return len(self.container)
+		if len(self.container) == 0:
+			return 0
+		return len(self.container[list(self.container.keys())[0]])
 
 	def items(self):
 		return self.container.items()
@@ -73,42 +75,72 @@ class TrainingHistory(BaseCallback):
 
 	@staticmethod
 	def _set_default_plot_kwargs(kwargs: dict):
-		kwargs.setdefault('fontsize', 16)
-		kwargs.setdefault('linewidth', 3)
+		kwargs.setdefault('fontsize', 18)
+		kwargs.setdefault('linewidth', 4)
 		kwargs.setdefault('figsize', (16, 12))
 		kwargs.setdefault('dpi', 300)
 		return kwargs
+
+	def create_plot(self, **kwargs) -> Tuple[plt.Figure, Dict[str, plt.Axes], Dict[str, plt.Line2D]]:
+		kwargs = self._set_default_plot_kwargs(kwargs)
+		loss_metrics = [k for k in self.container if 'loss' in k.lower()]
+		other_metrics = [k for k in self.container if k not in loss_metrics]
+		n_cols = int(np.sqrt(1 + len(other_metrics)))
+		n_rows = int(np.ceil((1 + len(other_metrics)) / n_cols))
+		axes_dict, lines = {}, {}
+		fig, axes = plt.subplots(nrows=n_rows, ncols=n_cols, figsize=kwargs["figsize"], sharex='all')
+		axes = np.ravel(axes)
+		for i, ax in enumerate(axes):
+			if i >= 1 + len(other_metrics):
+				ax.axis('off')
+				continue
+			if i == 0:
+				for k in loss_metrics:
+					lines[k] = ax.plot(self[k], label=k, linewidth=kwargs['linewidth'])[0]
+				axes_dict['losses'] = ax
+				ax.set_ylabel("Loss [-]", fontsize=kwargs["fontsize"])
+				ax.set_xlabel("Iterations [-]", fontsize=kwargs["fontsize"])
+				ax.legend(fontsize=kwargs["fontsize"])
+			else:
+				k = other_metrics[i - 1]
+				lines[k] = ax.plot(self[k], label=k, linewidth=kwargs['linewidth'])[0]
+				axes_dict[k] = ax
+				ax.set_xlabel("Iterations [-]", fontsize=kwargs["fontsize"])
+				ax.legend(fontsize=kwargs["fontsize"])
+		return fig, axes_dict, lines
 
 	def plot(
 			self,
 			save_path=None,
 			show=False,
 			**kwargs
-	):
+	) -> Tuple[plt.Figure, Dict[str, plt.Axes], Dict[str, plt.Line2D]]:
 		kwargs = self._set_default_plot_kwargs(kwargs)
-		loss_metrics = [k for k in self.container if 'loss' in k.lower()]
-		other_metrics = [k for k in self.container if k not in loss_metrics]
-		n_cols = int(np.sqrt(1 + len(other_metrics)))
-		n_rows = int((1 + len(other_metrics)) / n_cols)
-		fig, axes = plt.subplots(nrows=n_rows, ncols=n_cols, figsize=kwargs["figsize"], sharex='all')
-		axes = np.ravel(axes)
-		for i, ax in enumerate(axes):
-			if i == 0:
-				for k in loss_metrics:
-					ax.plot(self[k], label=k, linewidth=kwargs['linewidth'])
-				ax.set_ylabel("Loss [-]", fontsize=kwargs["fontsize"])
-				ax.set_xlabel("Iterations [-]", fontsize=kwargs["fontsize"])
-				ax.legend(fontsize=kwargs["fontsize"])
-			else:
-				k = other_metrics[i - 1]
-				ax.plot(self[k], label=k, linewidth=kwargs['linewidth'])
-				ax.set_xlabel("Iterations [-]", fontsize=kwargs["fontsize"])
-				ax.legend(fontsize=kwargs["fontsize"])
+		fig, axes, lines = self.create_plot(**kwargs)
 		if save_path is not None:
-			plt.savefig(save_path, dpi=kwargs["dpi"])
+			fig.savefig(save_path, dpi=kwargs["dpi"])
 		if show:
-			plt.show()
-		plt.close(fig)
+			plt.show(block=kwargs.get('block', True))
+		if kwargs.get('close', False):
+			plt.close(fig)
+		return fig, axes, lines
+
+	def update_fig(
+			self,
+			fig: plt.Figure,
+			axes: Dict[str, plt.Axes],
+			lines: Dict[str, plt.Line2D],
+			**kwargs
+	) -> Tuple[plt.Figure, Dict[str, plt.Axes], Dict[str, plt.Line2D]]:
+		kwargs = self._set_default_plot_kwargs(kwargs)
+		for k in lines:
+			lines[k].set_data(range(len(self[k])), self[k])
+		for k in axes:
+			axes[k].relim()
+			axes[k].autoscale_view()
+		fig.canvas.draw()
+		fig.canvas.flush_events()
+		return fig, axes, lines
 
 	def on_iteration_end(self, trainer):
 		self.concat(trainer.current_training_state.itr_metrics)

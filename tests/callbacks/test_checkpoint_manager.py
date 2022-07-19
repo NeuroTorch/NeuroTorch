b0a5685dd35173fdb31f1a0416ee7eb8aeae4a0e
Jérémie Gince <50332514+JeremieGince@users.noreply.github.com>
2022-07-18 23:19:06 -0400

    Update test_checkpoint_manager.py

--- tests/callbacks/test_checkpoint_manager.py
+++ tests/callbacks/test_checkpoint_manager.py
@@ -4,15 +4,27 @@ import shutil
 import unittest
 
 import numpy as np
-import psutil
 import torch
 
-from neurotorch.callbacks import CheckpointManager
+from neurotorch.callbacks import CheckpointManager, LoadCheckpointMode
+from neurotorch.modules import LIFLayer, SequentialModel
+from neurotorch.trainers.trainer import CurrentTrainingState
 
 
 class MockHistory:
 	def __init__(self):
-		pass
+		self.min_call_flag = False
+		self.max_call_flag = False
+		
+	def reset_mock(self):
+		self.min_call_flag = False
+		self.max_call_flag = False
+	
+	def min(self, *args, **kwargs):
+		self.min_call_flag = True
+		
+	def max(self, *args, **kwargs):
+		self.max_call_flag = True
 
 
 class MockTrainer:
@@ -20,7 +32,10 @@ class MockTrainer:
 		self.training_history = MockHistory()
 		self.callbacks = [self.training_history]
 		self.sort_flag = False
-
+		self.load_checkpoint_mode = None
+		self.force_overwrite = False
+		self.current_training_state = CurrentTrainingState.get_null_state()
+	
 	def sort_callbacks_(self):
 		self.sort_flag = True
 
@@ -41,7 +56,7 @@ class TestCheckpointManager(unittest.TestCase):
 		self.assertEqual(checkpoint_manager.metric, "val_loss")
 		self.assertEqual(checkpoint_manager.minimise_metric, True)
 		self.assertEqual(checkpoint_manager.curr_best_metric, np.inf)
-
+	
 	def test_constructor_with_args(self):
 		"""
 		Test that the default constructor works as expected.
@@ -63,7 +78,7 @@ class TestCheckpointManager(unittest.TestCase):
 		self.assertEqual(checkpoint_manager.metric, "test_metric")
 		self.assertEqual(checkpoint_manager.minimise_metric, False)
 		self.assertEqual(checkpoint_manager.curr_best_metric, -np.inf)
-
+	
 	def test_replace_trainer_history(self):
 		trainer = MockTrainer()
 		prev_len = len(trainer.callbacks)
@@ -76,12 +91,12 @@ class TestCheckpointManager(unittest.TestCase):
 		self.assertNotIn(prev_history, trainer.callbacks)
 		self.assertEqual(len(trainer.callbacks), prev_len)
 		self.assertTrue(trainer.sort_flag)
-
+	
 	def test_checkpoints_meta_path_property(self):
 		checkpoint_manager = CheckpointManager("./temp")
 		self.assertIsInstance(checkpoint_manager.checkpoints_meta_path, str)
 		self.assertTrue(checkpoint_manager.checkpoints_meta_path.endswith('.json'))
-
+	
 	def test_get_checkpoint_filename(self):
 		CheckpointManager.SAVE_EXT = '.pth'
 		checkpoint_manager = CheckpointManager("./temp")
@@ -89,7 +104,7 @@ class TestCheckpointManager(unittest.TestCase):
 		self.assertTrue(checkpoint_manager.get_checkpoint_filename().endswith(CheckpointManager.SAVE_EXT))
 		for i in range(10):
 			self.assertIn(str(i), checkpoint_manager.get_checkpoint_filename(i))
-
+	
 	def test_create_new_checkpoint_meta(self):
 		checkpoint_manager = CheckpointManager("./temp")
 		self.assertIsInstance(checkpoint_manager._create_new_checkpoint_meta(0), dict)
@@ -100,15 +115,15 @@ class TestCheckpointManager(unittest.TestCase):
 			checkpoint_manager._create_new_checkpoint_meta(0, best=True)[CheckpointManager.CHECKPOINT_BEST_KEY],
 			str
 		)
-
+	
 	def test_save_checkpoints_meta(self):
 		# clear the cache before running the test
 		if os.path.exists('./temp'):
 			shutil.rmtree("./temp")
-
+		
 		# create a new checkpoint manager
 		checkpoint_manager = CheckpointManager("./temp")
-
+		
 		# check the first checkpoint meta is saved
 		test_data = {'test_key': 'test_value'}
 		try:
@@ -121,7 +136,7 @@ class TestCheckpointManager(unittest.TestCase):
 		finally:
 			if os.path.exists('./temp'):
 				shutil.rmtree("./temp")
-
+		
 		# check that the updated checkpoint meta is saved
 		new_test_data = {'test_key_sec': 'test_value_sec'}
 		try:
@@ -135,20 +150,20 @@ class TestCheckpointManager(unittest.TestCase):
 		finally:
 			if os.path.exists('./temp'):
 				shutil.rmtree("./temp")
-
+	
 	def test_save_checkpoint_best(self):
 		# clear the cache before running the test
 		if os.path.exists('./temp'):
 			shutil.rmtree("./temp")
-
+		
 		# create a new checkpoint manager
 		checkpoint_manager = CheckpointManager("./temp")
 		test_data = {
-			CheckpointManager.CHECKPOINT_ITR_KEY: 0,
-			CheckpointManager.CHECKPOINT_METRICS_KEY: 'itr_metrics',
-			CheckpointManager.CHECKPOINT_STATE_DICT_KEY: {'test_key': 'test_value'},
+			CheckpointManager.CHECKPOINT_ITR_KEY                 : 0,
+			CheckpointManager.CHECKPOINT_METRICS_KEY             : 'itr_metrics',
+			CheckpointManager.CHECKPOINT_STATE_DICT_KEY          : {'test_key': 'test_value'},
 			CheckpointManager.CHECKPOINT_OPTIMIZER_STATE_DICT_KEY: {'test_key_opt': 'test_value_opt'},
-			CheckpointManager.CHECKPOINT_TRAINING_HISTORY_KEY: 'training_history',
+			CheckpointManager.CHECKPOINT_TRAINING_HISTORY_KEY    : 'training_history',
 		}
 		try:
 			save_path = checkpoint_manager.save_checkpoint(
@@ -172,20 +187,20 @@ class TestCheckpointManager(unittest.TestCase):
 		finally:
 			if os.path.exists('./temp'):
 				shutil.rmtree("./temp")
-
+	
 	def test_save_checkpoint(self):
 		# clear the cache before running the test
 		if os.path.exists('./temp'):
 			shutil.rmtree("./temp")
-
+		
 		# create a new checkpoint manager
 		checkpoint_manager = CheckpointManager("./temp")
 		test_data = {
-			CheckpointManager.CHECKPOINT_ITR_KEY: 0,
-			CheckpointManager.CHECKPOINT_METRICS_KEY: 'itr_metrics',
-			CheckpointManager.CHECKPOINT_STATE_DICT_KEY: {'test_key': 'test_value'},
+			CheckpointManager.CHECKPOINT_ITR_KEY                 : 0,
+			CheckpointManager.CHECKPOINT_METRICS_KEY             : 'itr_metrics',
+			CheckpointManager.CHECKPOINT_STATE_DICT_KEY          : {'test_key': 'test_value'},
 			CheckpointManager.CHECKPOINT_OPTIMIZER_STATE_DICT_KEY: {'test_key_opt': 'test_value_opt'},
-			CheckpointManager.CHECKPOINT_TRAINING_HISTORY_KEY: 'training_history',
+			CheckpointManager.CHECKPOINT_TRAINING_HISTORY_KEY    : 'training_history',
 		}
 		try:
 			save_path = checkpoint_manager.save_checkpoint(
@@ -209,12 +224,12 @@ class TestCheckpointManager(unittest.TestCase):
 		finally:
 			if os.path.exists('./temp'):
 				shutil.rmtree("./temp")
-
+	
 	def test_save_checkpoint_multiple_save(self):
 		# clear the cache before running the test
 		if os.path.exists('./temp'):
 			shutil.rmtree("./temp")
-
+		
 		# create a new checkpoint manager
 		checkpoint_manager = CheckpointManager("./temp")
 		try:
@@ -223,11 +238,11 @@ class TestCheckpointManager(unittest.TestCase):
 			for i in range(10):
 				best = i % 2 == 0
 				test_data = {
-					CheckpointManager.CHECKPOINT_ITR_KEY: i,
-					CheckpointManager.CHECKPOINT_METRICS_KEY: 'itr_metrics',
-					CheckpointManager.CHECKPOINT_STATE_DICT_KEY: {'test_key': 'test_value'},
+					CheckpointManager.CHECKPOINT_ITR_KEY                 : i,
+					CheckpointManager.CHECKPOINT_METRICS_KEY             : 'itr_metrics',
+					CheckpointManager.CHECKPOINT_STATE_DICT_KEY          : {'test_key': 'test_value'},
 					CheckpointManager.CHECKPOINT_OPTIMIZER_STATE_DICT_KEY: {'test_key_opt': 'test_value_opt'},
-					CheckpointManager.CHECKPOINT_TRAINING_HISTORY_KEY: 'training_history',
+					CheckpointManager.CHECKPOINT_TRAINING_HISTORY_KEY    : 'training_history',
 				}
 				save_path = checkpoint_manager.save_checkpoint(
 					itr=test_data[CheckpointManager.CHECKPOINT_ITR_KEY],
@@ -241,12 +256,12 @@ class TestCheckpointManager(unittest.TestCase):
 				saved_data = torch.load(save_path)
 				self.assertEqual(saved_data, test_data)
 				save_name = checkpoint_manager.get_checkpoint_filename(test_data[CheckpointManager.CHECKPOINT_ITR_KEY])
-
+				
 				path_list.append(save_path)
 				save_name_list.append(save_name)
 				self.assertEqual(len(set(path_list)), len(path_list))
 				self.assertEqual(len(set(save_name_list)), len(save_name_list))
-
+				
 				with open(checkpoint_manager.checkpoints_meta_path, "r") as jsonFile:
 					meta_info = json.load(jsonFile)
 				self.assertEqual(meta_info[CheckpointManager.CHECKPOINT_ITRS_KEY][str(i)], save_name)
@@ -254,31 +269,35 @@ class TestCheckpointManager(unittest.TestCase):
 					self.assertEqual(meta_info[CheckpointManager.CHECKPOINT_BEST_KEY], save_name)
 				else:
 					self.assertNotEqual(meta_info.get(CheckpointManager.CHECKPOINT_BEST_KEY), save_name)
-
+				
 				for j in range(i):
 					self.assertEqual(meta_info[CheckpointManager.CHECKPOINT_ITRS_KEY][str(j)], save_name_list[j])
 					self.assertTrue(os.path.exists(path_list[j]))
-
+		
 		except Exception as e:
 			self.fail(f"Exception raised: {e}")
 		finally:
 			if os.path.exists('./temp'):
 				shutil.rmtree("./temp")
-
-	def test_load_checkpoint_best(self):
-		raise NotImplementedError()
+	
+	def test_load_checkpoint_layer_best(self):
 		# clear the cache before running the test
 		if os.path.exists('./temp'):
 			shutil.rmtree("./temp")
-
+		
+		# create model est optimizer
+		model = LIFLayer(10, 10)
+		model.build()
+		opt = torch.optim.Adam(model.parameters(), lr=0.1)
+		
 		# create a new checkpoint manager
 		checkpoint_manager = CheckpointManager("./temp")
 		test_data = {
-			CheckpointManager.CHECKPOINT_ITR_KEY: 0,
-			CheckpointManager.CHECKPOINT_METRICS_KEY: 'itr_metrics',
-			CheckpointManager.CHECKPOINT_STATE_DICT_KEY: {'test_key': 'test_value'},
-			CheckpointManager.CHECKPOINT_OPTIMIZER_STATE_DICT_KEY: {'test_key_opt': 'test_value_opt'},
-			CheckpointManager.CHECKPOINT_TRAINING_HISTORY_KEY: 'training_history',
+			CheckpointManager.CHECKPOINT_ITR_KEY                 : 0,
+			CheckpointManager.CHECKPOINT_METRICS_KEY             : 'itr_metrics',
+			CheckpointManager.CHECKPOINT_STATE_DICT_KEY          : model.state_dict(),
+			CheckpointManager.CHECKPOINT_OPTIMIZER_STATE_DICT_KEY: opt.state_dict(),
+			CheckpointManager.CHECKPOINT_TRAINING_HISTORY_KEY    : 'training_history',
 		}
 		try:
 			save_path = checkpoint_manager.save_checkpoint(
@@ -289,16 +308,235 @@ class TestCheckpointManager(unittest.TestCase):
 				optimizer_state_dict=test_data[CheckpointManager.CHECKPOINT_OPTIMIZER_STATE_DICT_KEY],
 				training_history=test_data[CheckpointManager.CHECKPOINT_TRAINING_HISTORY_KEY]
 			)
-			self.assertTrue(save_path.endswith(CheckpointManager.SAVE_EXT))
-			saved_data = torch.load(save_path)
-			self.assertEqual(saved_data, test_data)
-			save_name = checkpoint_manager.get_checkpoint_filename(test_data[CheckpointManager.CHECKPOINT_ITR_KEY])
-			with open(checkpoint_manager.checkpoints_meta_path, "r") as jsonFile:
-				meta_info = json.load(jsonFile)
-			self.assertEqual(meta_info[CheckpointManager.CHECKPOINT_ITRS_KEY][str(0)], save_name)
-			self.assertEqual(meta_info[CheckpointManager.CHECKPOINT_BEST_KEY], save_name)
+			
+			checkpoint = checkpoint_manager.load_checkpoint(load_checkpoint_mode=LoadCheckpointMode.BEST_ITR)
+			self.assertTrue(
+				all(
+					torch.allclose(v, checkpoint[CheckpointManager.CHECKPOINT_STATE_DICT_KEY][k])
+					for k, v in model.state_dict().items()
+				)
+			)
+			self.assertTrue(
+				all(
+					np.allclose(v, second_param_group[k])
+					for first_param_group, second_param_group in zip(
+						opt.state_dict()['param_groups'],
+						checkpoint[CheckpointManager.CHECKPOINT_OPTIMIZER_STATE_DICT_KEY]['param_groups']
+					)
+					for k, v in first_param_group.items()
+				)
+			)
 		except Exception as e:
 			self.fail(f"Exception raised: {e}")
 		finally:
 			if os.path.exists('./temp'):
 				shutil.rmtree("./temp")
+	
+	def test_load_checkpoint_layer_not_best(self):
+		# clear the cache before running the test
+		if os.path.exists('./temp'):
+			shutil.rmtree("./temp")
+		
+		# create model est optimizer
+		model = LIFLayer(10, 10)
+		model.build()
+		opt = torch.optim.Adam(model.parameters(), lr=0.1)
+		
+		# create a new checkpoint manager
+		checkpoint_manager = CheckpointManager("./temp")
+		test_data = {
+			CheckpointManager.CHECKPOINT_ITR_KEY                 : 0,
+			CheckpointManager.CHECKPOINT_METRICS_KEY             : 'itr_metrics',
+			CheckpointManager.CHECKPOINT_STATE_DICT_KEY          : model.state_dict(),
+			CheckpointManager.CHECKPOINT_OPTIMIZER_STATE_DICT_KEY: opt.state_dict(),
+			CheckpointManager.CHECKPOINT_TRAINING_HISTORY_KEY    : 'training_history',
+		}
+		try:
+			save_path = checkpoint_manager.save_checkpoint(
+				itr=test_data[CheckpointManager.CHECKPOINT_ITR_KEY],
+				itr_metrics=test_data[CheckpointManager.CHECKPOINT_METRICS_KEY],
+				best=False,
+				state_dict=test_data[CheckpointManager.CHECKPOINT_STATE_DICT_KEY],
+				optimizer_state_dict=test_data[CheckpointManager.CHECKPOINT_OPTIMIZER_STATE_DICT_KEY],
+				training_history=test_data[CheckpointManager.CHECKPOINT_TRAINING_HISTORY_KEY]
+			)
+			
+			checkpoint = checkpoint_manager.load_checkpoint(load_checkpoint_mode=LoadCheckpointMode.LAST_ITR)
+			self.assertTrue(
+				all(
+					torch.allclose(v, checkpoint[CheckpointManager.CHECKPOINT_STATE_DICT_KEY][k])
+					for k, v in model.state_dict().items()
+				)
+			)
+			self.assertTrue(
+				all(
+					np.allclose(v, second_param_group[k])
+					for first_param_group, second_param_group in zip(
+						opt.state_dict()['param_groups'],
+						checkpoint[CheckpointManager.CHECKPOINT_OPTIMIZER_STATE_DICT_KEY]['param_groups']
+					)
+					for k, v in first_param_group.items()
+				)
+			)
+		except Exception as e:
+			self.fail(f"Exception raised: {e}")
+		finally:
+			if os.path.exists('./temp'):
+				shutil.rmtree("./temp")
+	
+	def test_load_checkpoint_sequential_best(self):
+		# clear the cache before running the test
+		if os.path.exists('./temp'):
+			shutil.rmtree("./temp")
+		
+		# create model est optimizer
+		model = SequentialModel(layers=[LIFLayer(10, 10), LIFLayer(10, 10), LIFLayer(10, 10)])
+		model.build()
+		opt = torch.optim.Adam(model.parameters(), lr=0.1)
+		
+		# create a new checkpoint manager
+		checkpoint_manager = CheckpointManager("./temp")
+		test_data = {
+			CheckpointManager.CHECKPOINT_ITR_KEY                 : 0,
+			CheckpointManager.CHECKPOINT_METRICS_KEY             : 'itr_metrics',
+			CheckpointManager.CHECKPOINT_STATE_DICT_KEY          : model.state_dict(),
+			CheckpointManager.CHECKPOINT_OPTIMIZER_STATE_DICT_KEY: opt.state_dict(),
+			CheckpointManager.CHECKPOINT_TRAINING_HISTORY_KEY    : 'training_history',
+		}
+		try:
+			save_path = checkpoint_manager.save_checkpoint(
+				itr=test_data[CheckpointManager.CHECKPOINT_ITR_KEY],
+				itr_metrics=test_data[CheckpointManager.CHECKPOINT_METRICS_KEY],
+				best=True,
+				state_dict=test_data[CheckpointManager.CHECKPOINT_STATE_DICT_KEY],
+				optimizer_state_dict=test_data[CheckpointManager.CHECKPOINT_OPTIMIZER_STATE_DICT_KEY],
+				training_history=test_data[CheckpointManager.CHECKPOINT_TRAINING_HISTORY_KEY]
+			)
+			
+			checkpoint = checkpoint_manager.load_checkpoint(load_checkpoint_mode=LoadCheckpointMode.BEST_ITR)
+			self.assertTrue(
+				all(
+					torch.allclose(v, checkpoint[CheckpointManager.CHECKPOINT_STATE_DICT_KEY][k])
+					for k, v in model.state_dict().items()
+				)
+			)
+			self.assertTrue(
+				all(
+					np.allclose(v, second_param_group[k])
+					for first_param_group, second_param_group in zip(
+						opt.state_dict()['param_groups'],
+						checkpoint[CheckpointManager.CHECKPOINT_OPTIMIZER_STATE_DICT_KEY]['param_groups']
+					)
+					for k, v in first_param_group.items()
+				)
+			)
+		except Exception as e:
+			self.fail(f"Exception raised: {e}")
+		finally:
+			if os.path.exists('./temp'):
+				shutil.rmtree("./temp")
+	
+	def test_load_checkpoint_sequential_not_best(self):
+		# clear the cache before running the test
+		if os.path.exists('./temp'):
+			shutil.rmtree("./temp")
+		
+		# create model est optimizer
+		model = SequentialModel(layers=[LIFLayer(10, 10), LIFLayer(10, 10), LIFLayer(10, 10)])
+		model.build()
+		opt = torch.optim.Adam(model.parameters(), lr=0.1)
+		
+		# create a new checkpoint manager
+		checkpoint_manager = CheckpointManager("./temp")
+		test_data = {
+			CheckpointManager.CHECKPOINT_ITR_KEY                 : 0,
+			CheckpointManager.CHECKPOINT_METRICS_KEY             : 'itr_metrics',
+			CheckpointManager.CHECKPOINT_STATE_DICT_KEY          : model.state_dict(),
+			CheckpointManager.CHECKPOINT_OPTIMIZER_STATE_DICT_KEY: opt.state_dict(),
+			CheckpointManager.CHECKPOINT_TRAINING_HISTORY_KEY    : 'training_history',
+		}
+		try:
+			save_path = checkpoint_manager.save_checkpoint(
+				itr=test_data[CheckpointManager.CHECKPOINT_ITR_KEY],
+				itr_metrics=test_data[CheckpointManager.CHECKPOINT_METRICS_KEY],
+				best=False,
+				state_dict=test_data[CheckpointManager.CHECKPOINT_STATE_DICT_KEY],
+				optimizer_state_dict=test_data[CheckpointManager.CHECKPOINT_OPTIMIZER_STATE_DICT_KEY],
+				training_history=test_data[CheckpointManager.CHECKPOINT_TRAINING_HISTORY_KEY]
+			)
+			
+			checkpoint = checkpoint_manager.load_checkpoint(load_checkpoint_mode=LoadCheckpointMode.LAST_ITR)
+			self.assertTrue(
+				all(
+					torch.allclose(v, checkpoint[CheckpointManager.CHECKPOINT_STATE_DICT_KEY][k])
+					for k, v in model.state_dict().items()
+				)
+			)
+			self.assertTrue(
+				all(
+					np.allclose(v, second_param_group[k])
+					for first_param_group, second_param_group in zip(
+						opt.state_dict()['param_groups'],
+						checkpoint[CheckpointManager.CHECKPOINT_OPTIMIZER_STATE_DICT_KEY]['param_groups']
+					)
+					for k, v in first_param_group.items()
+				)
+			)
+		except Exception as e:
+			self.fail(f"Exception raised: {e}")
+		finally:
+			if os.path.exists('./temp'):
+				shutil.rmtree("./temp")
+
+	def test_start_overwrite_minimise(self):
+		# clear the cache before running the test
+		if os.path.exists('./temp'):
+			shutil.rmtree("./temp")
+		try:
+			trainer = MockTrainer()
+			trainer.load_checkpoint_mode = None
+			trainer.force_overwrite = True
+			checkpoint_manager = CheckpointManager("./temp", minimise_metric=True)
+			checkpoint_manager.save_checkpoints_meta({"test": "test"})
+			self.assertTrue(os.path.exists("./temp"))
+			checkpoint_manager.start(trainer)
+			self.assertFalse(os.path.exists("./temp"))
+			self.assertEqual(trainer.current_training_state.iteration, 0)
+			self.assertTrue(trainer.training_history.min_call_flag)
+			self.assertFalse(trainer.training_history.max_call_flag)
+		except Exception as e:
+			self.fail(f"Exception raised: {e}")
+		finally:
+			if os.path.exists('./temp'):
+				shutil.rmtree("./temp")
+	
+	def test_start_overwrite_maximise(self):
+		# clear the cache before running the test
+		if os.path.exists('./temp'):
+			shutil.rmtree("./temp")
+		try:
+			trainer = MockTrainer()
+			trainer.load_checkpoint_mode = None
+			trainer.force_overwrite = True
+			checkpoint_manager = CheckpointManager("./temp", minimise_metric=False)
+			checkpoint_manager.save_checkpoints_meta({"test": "test"})
+			self.assertTrue(os.path.exists("./temp"))
+			checkpoint_manager.start(trainer)
+			self.assertFalse(os.path.exists("./temp"))
+			self.assertEqual(trainer.current_training_state.iteration, 0)
+			self.assertFalse(trainer.training_history.min_call_flag)
+			self.assertTrue(trainer.training_history.max_call_flag)
+		except Exception as e:
+			self.fail(f"Exception raised: {e}")
+		finally:
+			if os.path.exists('./temp'):
+				shutil.rmtree("./temp")
+
+
+
+
+
+
+
+
+

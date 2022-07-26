857f9ddd9bdf341441072a754a8f7b6b4deb08d0
Jérémie Gince <50332514+JeremieGince@users.noreply.github.com>
2022-07-26 12:07:46 -0400

    Update generate_figures.py

--- applications/mnist/generate_figures.py
+++ applications/mnist/generate_figures.py
@@ -1,7 +1,12 @@
+from functools import partial
+from typing import Dict, List, Optional
+
 import pandas as pd
 import plotly.graph_objects as go
 import seaborn as sns
 import numpy as np
+
+from neurotorch.modules import LayerType
 
 plot_layout = dict(
 	paper_bgcolor='rgb(243, 243, 243)',
@@ -28,14 +33,26 @@ plot_layout = dict(
 )
 
 dict_param_name = {
-	'hidden_layer_type': 'Dynamique',
-	"use_recurrent_connection": "Connections récurrentes",
-	"to_spikes_use_periods": 'Temps en période',
-	"n_hidden_neurons": 'Taille de la couche cachée',
-	"n_iteration": "Nombre d'itérations",
-	"learn_beta": "Apprentissage de Beta",
-	"n_steps": "Temps d'intégration",
+	'input_layer_type': 'Input Dynamics',
+	'hidden_layer_type': 'Hidden Dynamics',
+	'readout_layer_type': 'Readout Dynamics',
+	"use_recurrent_connection": "Recurrent Connection",
+	"to_spikes_use_periods": 'Time as periods',
+	"n_hidden_neurons": 'Size of hidden layer',
+	"n_iteration": "Number of iterations",
+	"learn_beta": "Learning of beta",
+	"n_steps": "Integration Time",
 }
+dict_param_surname = dict(
+	input_layer_type='I: ',
+	hidden_layer_type='H: ',
+	readout_layer_type='O: ',
+	use_recurrent_connection='R ',
+	to_spikes_use_periods='P ',
+	n_hidden_neurons='N ',
+	learn_beta='B ',
+	n_steps='T ',
+)
 
 
 def load_results(file_path='tr_data/results.csv') -> pd.DataFrame:
@@ -52,7 +69,8 @@ def _plot_bar_result(
 		y_axis: str,
 		data_mask: tuple = None,
 		list_col_names=None,
-		**kwargs):
+		**kwargs
+):
 	"""
 	Plots a histogram of the given results.
 	"""
@@ -77,21 +95,14 @@ def _plot_bar_result(
 	# 		col_names.remove(hide_col)
 	dataset_results = dataset_results.sort_values(
 		by=col_names,
-		ignore_index=True)
+		ignore_index=True
+	)
 	if data_mask is not None:
 		dataset_results = dataset_results[dataset_results[data_mask[0]] == data_mask[1]]
 
 	y_data = dataset_results[y_axis] * 100
 	y_data = y_data.tolist()
 	xlabel = []
-	dict_param_surname = dict(
-		hidden_layer_type='',
-		use_recurrent_connection='R ',
-		to_spikes_use_periods='P ',
-		n_hidden_neurons='H ',
-		learn_beta='B ',
-		n_steps='T ',
-	)
 	for i in range(dataset_results.shape[0]):
 		label = ''
 		for col in list_col_names:
@@ -193,18 +204,13 @@ def make_data_for_box_plot(results: pd.DataFrame, dataset_name: str, ydata: str)
 		if len(dataset_results[param].unique()) <= 1:
 			continue
 		for param_value in dataset_results[param].unique():
-			if param == 'hidden_layer_type':
-				param_value_name = param_value.split('.')[-1]
-			elif param == 'use_recurrent_connection':
-				param_value_name = 'REC [✓]' if param_value else 'REC [X]'
-			elif param == 'to_spikes_use_periods':
-				param_value_name = 'P [✓]' if param_value else 'P [X]'
-			elif param == 'n_hidden_neurons':
-				param_value_name = f'HN {param_value}'
-			elif param == 'n_steps':
-				param_value_name = f'T {param_value}'
+			surname = dict_param_surname.get(param, '')
+			if isinstance(param_value, bool):
+				param_value_name = f"{surname}[✓]" if param_value else f"{surname}[X]"
+			elif isinstance(param_value, (str, LayerType)) and LayerType.from_str(param_value) is not None:
+				param_value_name = f"{surname}{LayerType.from_str(param_value).name}"
 			else:
-				param_value_name = param_value
+				param_value_name = f"{surname}{param_value}"
 			try:
 				box_plot_data[f'{param_value_name}'] = y_data[dataset_results[param] == param_value].tolist()
 			except ValueError as err:
@@ -248,23 +254,106 @@ def box_plot_accuracy(results: pd.DataFrame, dataset_name: str):
 	return figure
 
 
+def format_table(
+		df: pd.DataFrame,
+		cols: Optional[List[str]] = None,
+		cols_to_percent: Optional[List[str]] = None,
+		cols_rename: Optional[Dict[str, str]] = None,
+		n_digits: int = 2,
+		checkmark_as_latex_command: bool = True,
+) -> pd.DataFrame:
+	formatted_table = df.copy()
+	if cols is None:
+		cols = formatted_table.columns
+	formatted_table = formatted_table[cols]
+	for layer_type in LayerType:
+		formatted_table = formatted_table.replace(f"{layer_type}", layer_type.name, regex=True)
+	# checkmark_symbol = r'\u2713' if checkmark_as_latex_command else '✓'
+	checkmark_symbol = '$\\checkmark$' if checkmark_as_latex_command else '✓'
+	formatted_table = formatted_table.replace(True, checkmark_symbol, regex=True)
+	formatted_table = formatted_table.replace(False, "x", regex=True)
+	if cols_to_percent is not None:
+		for col in cols_to_percent:
+			formatted_table[col] = formatted_table[col] * 100
+			formatted_table[col] = formatted_table[col].round(n_digits)
+			max_value = formatted_table[col].max()
+			formatted_table[col] = formatted_table[col].astype(str)
+			formatted_table[col] = formatted_table[col].replace(f'{max_value}', f"\\textbf{{{max_value}}}", regex=True)
+	if cols_rename is not None:
+		formatted_table.rename(columns=cols_rename, inplace=True)
+	formatted_table = formatted_table.rename(
+		{c_name: c_name.replace('_', ' ') for c_name in formatted_table.columns},
+		axis='columns'
+	)
+	return formatted_table
+
+
+def format_table_metric_value(
+		x,
+		bold_value: float = np.inf,
+		n_digits: int = 2,
+):
+	x = round(100*x, n_digits)
+	if np.isclose(x, bold_value):
+		x = f"\\textbf{{{x}}}"
+	return x
+
+
 if __name__ == '__main__':
-	result = load_results('tr_data/results.csv')
-	best_result = result.sort_values(by='test_accuracy', ascending=False).iloc[0]
-	print(f"f{best_result = }")
-	# for _dataset_name_ in ['MNIST', 'FASHION_MNIST']:
+	result = load_results('tr_data_001/results.csv')
+	best_result = result.sort_values(by='test_accuracy', ascending=False).iloc[:3]
+	cols_oi = [
+		'input_layer_type',
+		'hidden_layer_type',
+		'readout_layer_type',
+		'use_recurrent_connection',
+		'n_steps',
+		"test_accuracy",
+		'test_f1',
+		'test_precision',
+		'test_recall'
+	]
+	metrics_cols = [
+		'test_accuracy',
+		'test_f1',
+		'test_precision',
+		'test_recall'
+	]
+	cols_rename = {
+		'input_layer_type': 'Input layer',
+		'hidden_layer_type': 'Hidden layer',
+		'readout_layer_type': 'Readout layer',
+		'use_recurrent_connection': 'Recurrent connection',
+		'n_steps': 'N steps',
+		'test_accuracy': 'Accuracy',
+		'test_f1': 'F1',
+		'test_precision': 'Precision',
+		'test_recall': 'Recall',
+	}
+	filtered_result = format_table(result, cols_oi, metrics_cols, cols_rename)
+	filtered_best_result = format_table(best_result, cols_oi, metrics_cols, cols_rename)
+	with pd.option_context('display.max_rows', None, 'display.max_columns', None):
+		print(f"result:\n{filtered_result.to_latex(index=False, escape=False)}")
+	with pd.option_context('display.max_rows', None, 'display.max_columns', None):
+		print(f"best_result:\n{filtered_best_result.to_latex(index=False, escape=False)}")
+	# for _dataset_name_ in [
+	# 	'MNIST',
+	# 	# 'FASHION_MNIST'
+	# ]:
 	# 	box_plot_accuracy(result, _dataset_name_).show()
 	# 	plot_bar_result(
 	# 		result,
 	# 		_dataset_name_,
 	# 		[
 	# 			'test_accuracy',
-	# 			'test_f1',
+	# 			# 'test_f1',
 	# 			# 'test_precision',
 	# 			# 'test_recall'
 	# 		],
 	# 		list_col_names_xaxis=[
+	# 			'input_layer_type',
 	# 			'hidden_layer_type',
+	# 			'readout_layer_type',
 	# 			'use_recurrent_connection',
 	# 			'n_steps',
 	# 		]

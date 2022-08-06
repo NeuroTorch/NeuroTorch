6740b1a1b7ec2e56f3b84168d6421b8b0f366c56
Jérémie Gince <50332514+JeremieGince@users.noreply.github.com>
2022-08-05 21:52:05 -0400

    Update ts_pred_with_lif_autoencoder.py

--- applications/time_series_forecasting_spiking/ts_pred_with_lif_autoencoder.py
+++ applications/time_series_forecasting_spiking/ts_pred_with_lif_autoencoder.py
@@ -7,9 +7,9 @@ from applications.time_series_forecasting_spiking.dataset import TimeSeriesDatas
 from applications.time_series_forecasting_spiking.lif_auto_encoder import train_auto_encoder, show_prediction
 from neurotorch.transforms import ConstantValuesTransform
 
-n_units = 2
-n_encoder_steps = 32
-n_iterations = 300
+n_units = 16
+n_encoder_steps = 4
+n_iterations = 100
 
 ws_ts = TimeSeriesDataset(
 	input_transform=ConstantValuesTransform(
@@ -24,17 +24,17 @@ ws_ts = TimeSeriesDataset(
 auto_encoder_training_output = train_auto_encoder(
 	n_units=n_units, n_encoder_steps=n_encoder_steps, batch_size=256, n_iterations=256
 )
-show_prediction(auto_encoder_training_output.dataset.data, auto_encoder_training_output.spikes_auto_encoder)
+# show_prediction(auto_encoder_training_output.dataset.data, auto_encoder_training_output.spikes_auto_encoder)
 # auto_encoder_training_output.history.plot(show=True)
 
 
 spikes_encoder = auto_encoder_training_output.spikes_auto_encoder.spikes_encoder
-spikes_encoder.learning_type = nt.LearningType.NONE
-spikes_encoder.requires_grad_(False)
+# spikes_encoder.learning_type = nt.LearningType.NONE
+# spikes_encoder.requires_grad_(False)
 spikes_decoder = auto_encoder_training_output.spikes_auto_encoder.spikes_decoder
-spikes_decoder.requires_grad_(False)
+# spikes_decoder.requires_grad_(False)
 
-lif_layer = nt.SpyLIFLayer(
+lif_layer = nt.ALIFLayer(
 	input_size=nt.Size(
 		[
 			nt.Dimension(None, nt.DimensionProperty.TIME),
@@ -46,10 +46,9 @@ lif_layer = nt.SpyLIFLayer(
 ).build()
 all_parameters = [
 	*list(lif_layer.parameters()),
-	# *list(spikes_encoder.parameters()),
+	*list(spikes_encoder.parameters()),
 	*list(spikes_decoder.parameters()),
 ]
-optimizer = torch.optim.Adam(all_parameters, lr=1e-3, maximize=True)
 criterion = nt.losses.PVarianceLoss()
 
 t0, target = ws_ts[0]
@@ -57,7 +56,7 @@ t0 = torch.unsqueeze(t0, 0)
 target = torch.unsqueeze(target, 0)
 
 
-def predict():
+def predict(network):
 	t0_spikes = []
 	hh_encoder = None
 	for t in range(n_encoder_steps):
@@ -71,12 +70,12 @@ def predict():
 	hh = None
 	for t in range(n_encoder_steps):
 		x = t0_spikes[:, t]
-		x, hh = lif_layer(x, hh)
+		x, hh = network(x, hh)
 		spikes_preds.append(x)
 	
 	for t in range((ws_ts.n_time_steps - 1) * n_encoder_steps):
 		x = spikes_preds[-1]
-		x, hh = lif_layer(x, hh)
+		x, hh = network(x, hh)
 		spikes_preds.append(x)
 	
 	ts_decode = spikes_decoder(torch.stack(spikes_preds, dim=1)[:, n_encoder_steps:])
@@ -113,18 +112,38 @@ def predict_spikes():
 	return preds
 
 
-def train():
+def train(network):
+	lr_schedule = [5e-2, 1e-2, 5e-3, 1e-3, 5e-4, 1e-4, 5e-5, 1e-5]
+	loss_schedule = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7]
+	curr_stage = 0
+	curr_lr = 1e-1
+	lr_history = []
+	history = []
+	optimizer = torch.optim.Adam(all_parameters, lr=curr_lr, maximize=True)
 	p_bar = tqdm.tqdm(range(n_iterations))
 	for i in p_bar:
-		preds = predict()
+		preds = predict(network)
 		loss = criterion(preds, target.to(preds.device))
 		optimizer.zero_grad()
 		loss.backward()
 		optimizer.step()
+		history.append(loss.detach().cpu().item())
+		lr_history.append(curr_lr)
 		p_bar.set_description(f"pVar: {loss.detach().cpu().item():.4f}")
 		if loss.detach().cpu().item() > 0.98:
 			p_bar.close()
 			break
+		if loss.detach().cpu().item() > loss_schedule[curr_stage]:
+			curr_stage += 1
+			if curr_stage < len(lr_schedule):
+				curr_lr = lr_schedule[curr_stage]
+				for g in optimizer.param_groups:
+					g['lr'] = curr_lr
+			else:
+				curr_lr = lr_schedule[-1]
+				for g in optimizer.param_groups:
+					g['lr'] = curr_lr
+	return history, lr_history
 
 # predictions = predict().detach().cpu()
 # errors = torch.squeeze(predictions - target.to(predictions.device))**2
@@ -158,14 +177,24 @@ def train():
 
 
 if __name__ == '__main__':
-	train()
+	hist, lr_hist = train(lif_layer)
+	fig, axes = plt.subplots(2, 1, figsize=(12, 8))
+	axes[0].plot(hist, label="Loss")
+	axes[0].set_xlabel("Iteration [-]")
+	axes[0].set_ylabel("Loss [-]")
+	axes[0].legend()
+	axes[1].plot(lr_hist, label="Learning rate")
+	axes[1].set_xlabel("Iteration [-]")
+	axes[1].set_ylabel("Learning rate [-]")
+	plt.show()
+	plt.close(fig)
 	
-	predictions = torch.squeeze(predict().detach().cpu())
+	predictions = torch.squeeze(predict(lif_layer).detach().cpu())
 	target = torch.squeeze(target.detach().cpu())
 	errors = torch.squeeze(predictions - target.to(predictions.device))**2
 	mse_loss = torch.nn.MSELoss()(predictions, target.to(predictions.device))
 	pVar = 1 - mse_loss / torch.var(target.to(mse_loss.device))
-	fig, axes = plt.subplots(3, 1, figsize=(15, 8))
+	fig, axes = plt.subplots(3, 1, figsize=(12, 8))
 	axes[0].plot(errors.detach().cpu().numpy())
 	axes[0].set_xlabel("Time [-]")
 	axes[0].set_ylabel("Squared Error [-]")

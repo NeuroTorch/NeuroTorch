d2e32c99b13e06fb1e2ca6401f8bf57e71ebc4e3
Jérémie Gince <50332514+JeremieGince@users.noreply.github.com>
2022-08-17 21:45:13 -0400

    Update __init__.py

--- src/neurotorch/transforms/__init__.py
+++ src/neurotorch/transforms/__init__.py
@@ -3,6 +3,17 @@ from .base import (
 	to_tensor,
 	LinearRateToSpikes,
 	ConstantValuesTransform,
+	IdentityTransform,
+	ToDevice,
+	ToTensor,
 )
+
+# from .spikes_encoders import (
+# 	SpikesEncoder,
+# 	LIFEncoder,
+# 	SpyLIFLayer,
+# 	ALIFEncoder,
+# )
+
 
 

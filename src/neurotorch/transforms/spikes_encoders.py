bb9a04bf66b4a8c9a68520e18f6ef009d223cf34
Jérémie Gince <50332514+JeremieGince@users.noreply.github.com>
2022-08-17 21:45:25 -0400

    Update spikes_encoders.py

--- src/neurotorch/transforms/spikes_encoders.py
+++ src/neurotorch/transforms/spikes_encoders.py
@@ -4,7 +4,7 @@ import numpy as np
 import torch
 
 from . import ConstantValuesTransform, to_tensor
-from .. import LIFLayer, SpyLIFLayer, ALIFLayer, LearningType
+from ..modules.layers import LIFLayer, SpyLIFLayer, ALIFLayer, LearningType
 
 
 class SpikesEncoder(torch.nn.Module):
@@ -14,21 +14,42 @@ class SpikesEncoder(torch.nn.Module):
 			n_units: int,
 			spikes_layer_type: Type[Union[LIFLayer, SpyLIFLayer, ALIFLayer]],
 			dt: float = 1e-3,
+			learning_type: LearningType = LearningType.NONE,
 			device: Optional[torch.device] = None,
 			spikes_layer_kwargs: Optional[dict] = None,
 	):
+		"""
+		Constructor for the SpikesEncoder class.
+		
+		:param n_steps: Number of steps to encode.
+		:param n_units: Number of units in the encoder.
+		:param spikes_layer_type: Dynamic of the spikes layer.
+		:param dt: Time step of the simulation.
+		:param learning_type: Learning type of the spikes layer.
+		:param device: Device to use for the encoder.
+		:param spikes_layer_kwargs: Keyword arguments for the spikes layer.
+		
+		:keyword spikes_layer_kwargs:
+			* <forward_weights>: Forward weights of the spikes layer. Default: eye(n_units).
+			* <use_recurrent_connection>: Whether to use a recurrent connection. Default: False.
+		
+		"""
 		super().__init__()
 		self.n_steps = n_steps
-		self.const_transform = ConstantValuesTransform(n_steps)
+		self.const_transform = ConstantValuesTransform(n_steps, batch_wise=True)
 		self.spikes_layer_type = spikes_layer_type
 		self.dt = dt
+		self.learning_type = learning_type
 		if spikes_layer_kwargs is None:
-			spikes_layer_kwargs = {}
+			spikes_layer_kwargs = dict(
+				forward_weights=torch.eye(n_units),
+				use_recurrent_connection=False,
+			)
 		
 		assert "forward_weights" not in spikes_layer_kwargs, \
 			"forward_weights cannot be specified since it must be eye(n_units)"
 		assert "learning_type" not in spikes_layer_kwargs, \
-			"learning_type cannot be specified since it must be LearningType.NONE"
+			"learning_type cannot be specified since it must be the given value"
 		assert "use_recurrent_connection" not in spikes_layer_kwargs, \
 			"use_recurrent_connection cannot be specified since it must be False"
 		assert "dt" not in spikes_layer_kwargs, \
@@ -38,9 +59,7 @@ class SpikesEncoder(torch.nn.Module):
 		
 		self.spikes_layer = self.spikes_layer_type(
 			n_units, n_units,
-			forward_weights=torch.eye(n_units),
-			use_recurrent_connection=False,
-			learning_type=LearningType.NONE,
+			learning_type=self.learning_type,
 			dt=dt,
 			device=device,
 			**spikes_layer_kwargs,
@@ -49,14 +68,13 @@ class SpikesEncoder(torch.nn.Module):
 
 	def forward(self, x: Any):
 		x = to_tensor(x).to(self.spikes_layer.device)
-		x = self.const_transform(x)[np.newaxis, :]
-		x_spikes = []
-		hh = None
+		x = self.const_transform(x)
+		x_spikes, hh = [], None
 		for t in range(self.n_steps):
 			spikes, hh = self.spikes_layer(x[:, t], hh)
 			x_spikes.append(spikes)
-		x_spikes = torch.squeeze(torch.stack(x_spikes, dim=1))
-		return x_spikes
+		# x_spikes = torch.squeeze(torch.stack(x_spikes, dim=1))
+		return torch.stack(x_spikes, dim=1)
 
 
 class LIFEncoder(SpikesEncoder):
@@ -71,7 +89,7 @@ class LIFEncoder(SpikesEncoder):
 		super().__init__(
 			n_steps, n_units, LIFLayer, dt, device, spikes_layer_kwargs,
 		)
-		
+
 
 class SpyLIFEncoder(SpikesEncoder):
 	def __init__(

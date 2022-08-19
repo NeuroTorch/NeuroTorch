90658f4abf7ed6c0e8e380951a00a564b39f9a07
Jérémie Gince <50332514+JeremieGince@users.noreply.github.com>
2022-08-19 12:19:44 -0400

    Update main.py

--- applications/time_series_forecasting_spiking/main.py
+++ applications/time_series_forecasting_spiking/main.py
@@ -4,10 +4,11 @@ import numpy as np
 import torch
 from pythonbasictools import DeepLib, log_device_setup, logs_file_setup
 
-from applications.fit_wilson_cowan_with_lif.results_generation import train_with_params
+from applications.time_series_forecasting_spiking.results_generation import train_with_params
 from neurotorch.modules.layers import LayerType
 from neurotorch.transforms.spikes_encoders import LIFEncoder
 from neurotorch.utils import set_seed
+import neurotorch as nt
 
 if __name__ == '__main__':
 	logs_file_setup(__file__, add_stdout=False)
@@ -15,37 +16,27 @@ if __name__ == '__main__':
 	torch.autograd.set_detect_anomaly(True)
 	torch.cuda.set_per_process_memory_fraction(0.5)
 	
-	set_seed(42)
-	
-	n_units = 16
-	t_0 = np.random.rand(n_units)
-	forward_weights = 3 * np.random.randn(n_units, n_units)
-	mu = np.random.randn(n_units, )
-	r = np.random.rand(1).item()
-	tau = 1.0
+	seed = 0
+	set_seed(seed)
 	
 	results = train_with_params(
 		{
-			"n_steps": 10,
-			"n_encoder_steps": 8,
-			"n_units": n_units,
+			"dataset_name": "timeSeries_2020_12_16_cr3_df.npy",
+			"n_time_steps": 16,
+			"n_encoder_steps": 32,
+			"n_units": 128,
 			"dt": 2e-2,
 			"optimizer": "Adam",
-			"learning_rate": 1e-2,
-			"encoder": LIFEncoder,
-			"hidden_layer_type": LayerType.SpyLIF,
-			"readout_layer_type": LayerType.SpyLI,
+			"learning_rate": 5e-5,
+			"encoder_type": nt.SpyLIFLayer,
+			"use_recurrent_connection": True,
+			"seed": seed,
 		},
-		t_0=t_0,
-		forward_weights=forward_weights,
-		mu=mu,
-		r=r,
-		tau=tau,
-		n_iterations=100,
+		n_iterations=1024,
 		verbose=True,
 		show_training=False,
 		force_overwrite=True,
-		data_folder="tr_test",
+		data_folder="tr_results",
 	)
 	pprint.pprint(results, indent=4)
 	results["history"].plot(show=True)

4bb3413314aad46daca5dcda774f35583d581b75
Jérémie Gince <50332514+JeremieGince@users.noreply.github.com>
2022-08-22 23:15:27 -0400

    Update main.py

--- applications/time_series_forecasting_spiking/main.py
+++ applications/time_series_forecasting_spiking/main.py
@@ -1,17 +1,18 @@
 import pprint
 
+import matplotlib.pyplot as plt
 import torch
 from pythonbasictools import DeepLib, log_device_setup, logs_file_setup
 
-from applications.time_series_forecasting_spiking.results_generation import train_with_params
+from applications.time_series_forecasting_spiking.results_generation import train_with_params, visualize_forecasting
+from applications.time_series_forecasting_spiking.spikes_auto_encoder_training import visualize_reconstruction
 from neurotorch.utils import set_seed
 import neurotorch as nt
 
 if __name__ == '__main__':
 	logs_file_setup(__file__, add_stdout=False)
 	log_device_setup(deepLib=DeepLib.Pytorch)
-	torch.autograd.set_detect_anomaly(True)
-	torch.cuda.set_per_process_memory_fraction(0.8)
+	torch.cuda.set_per_process_memory_fraction(0.5)
 	
 	seed = 0
 	set_seed(seed)
@@ -22,19 +23,32 @@ if __name__ == '__main__':
 			"n_time_steps": 16,
 			"n_encoder_steps": 32,
 			"n_units": 128,
-			"dt": 2e-2,
-			"optimizer": "AdamW",
-			"learning_rate": 5e-5,
-			# "min_lr": 1e-6,
+			"dt": 1e-3,
+			"optimizer": "Adam",
+			"learning_rate": 8e-5,
+			"min_lr": 5e-6,
 			"encoder_type": nt.SpyLIFLayer,
 			"use_recurrent_connection": True,
 			"seed": seed,
+			"smoothing_sigma": 5,
 		},
-		n_iterations=1024,
+		n_iterations=4096,
 		verbose=True,
 		show_training=False,
 		force_overwrite=True,
-		data_folder="checkpoints",
+		data_folder="checkpoints_test_smoothing",
+		encoder_data_folder="encoder_checkpoints",
 	)
 	pprint.pprint(results, indent=4)
 	results["history"].plot(show=True)
+	visualize_reconstruction(
+		results["auto_encoder_training_output"].dataset.data,
+		results["auto_encoder_training_output"].spikes_auto_encoder,
+		show=True,
+	)
+	visualize_forecasting(
+		results["network"],
+		results["auto_encoder_training_output"].spikes_auto_encoder,
+		results["dataloader"],
+		show=True,
+	)

179aaf53966f51d89a87b858a6bd118acf12bb56
Jérémie Gince <50332514+JeremieGince@users.noreply.github.com>
2022-09-01 22:04:39 -0400

    try spnhinx and docs separatly

--- sphinx/generate_doc.py
+++ sphinx/generate_doc.py
@@ -6,6 +6,9 @@ def generate_doc():
 		r"sphinx-apidoc -f -o ./source ../src/neurotorch",
 		r".\make clean html",
 		r".\make html",
+		# r"rmdir ../docs",
+		# r"mkdir ../docs",
+		r"move ./build/html/* ../docs/"
 	]
 	for command in commands:
 		print(f"Executing: {command}")

ecdb08301176bd6c4645058a4a12368075d02a14
Jérémie Gince <50332514+JeremieGince@users.noreply.github.com>
2022-09-02 13:00:25 -0400

    Update conf.py

--- sphinx/source/conf.py
+++ sphinx/source/conf.py
@@ -5,6 +5,7 @@
 
 import os
 import sys
+import shutil
 basedir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', 'src'))
 sys.path.insert(0, basedir)
 
@@ -17,6 +18,37 @@ def skip(app, what, name, obj, would_skip, options):
 
 def setup(app):
     app.connect("autodoc-skip-member", skip)
+    app.connect('html-page-context', change_pathto)
+    app.connect('build-finished', move_private_folders)
+
+
+def change_pathto(app, pagename, templatename, context, doctree):
+    """
+    Replace pathto helper to change paths to folders with a leading underscore.
+    """
+    pathto = context.get('pathto')
+    
+    def gh_pathto(otheruri, *args, **kw):
+        if otheruri.startswith('_'):
+            otheruri = otheruri[1:]
+        return pathto(otheruri, *args, **kw)
+    
+    context['pathto'] = gh_pathto
+
+
+def move_private_folders(app, e):
+    """
+    remove leading underscore from folders in in the output folder.
+
+    :todo: should only affect html built
+    """
+    
+    def join(dir):
+        return os.path.join(app.builder.outdir, dir)
+    
+    for item in os.listdir(app.builder.outdir):
+        if item.startswith('_') and os.path.isdir(join(item)):
+            shutil.move(join(item), join(item[1:]))
 
 
 # -- Project information -----------------------------------------------------
@@ -29,7 +61,17 @@ author = 'Jeremie Gince'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon']
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.githubpages',
+    'sphinx_rtd_theme',
+    'sphinxtogithub',
+]
+
+sphinx_to_github = True
+sphinx_to_github_verbose = True
+sphinx_to_github_encoding = "utf-8"
 
 templates_path = ['_templates']
 exclude_patterns = []

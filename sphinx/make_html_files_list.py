2f19ff3d8c34a5ee80d9513a47efcd36f3f193b5
Jérémie Gince <50332514+JeremieGince@users.noreply.github.com>
2022-09-02 12:18:52 -0400

    Update make_html_files_list.py

--- sphinx/make_html_files_list.py
+++ sphinx/make_html_files_list.py
@@ -2,9 +2,10 @@ import os
 
 
 def make_html_file_list():
-	meta_filename = "docs/html_files_list.txt"
-	files = os.listdir(os.path.join(os.getcwd(), "build", "html"))
-	with open(meta_filename, "w+") as f:
+	meta_filename = "./docs/html_files_list.txt"
+	os.makedirs(os.path.dirname(meta_filename), exist_ok=True)
+	files = os.listdir(os.path.join('sphinx', "build", "html"))
+	with open(meta_filename, "w") as f:
 		f.write("\n".join(files))
 	
 

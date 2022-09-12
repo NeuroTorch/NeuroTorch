6658b42ba961b561db3f22df49775f825a43387e
Jérémie Gince <50332514+JeremieGince@users.noreply.github.com>
2022-09-12 02:31:46 -0400

    Update wrappers.py

--- src/neurotorch/transforms/wrappers.py
+++ src/neurotorch/transforms/wrappers.py
@@ -8,6 +8,9 @@ class CallableToModuleWrapper(torch.nn.Module):
 		super().__init__()
 		assert callable(callable_object), "The callable object must be callable."
 		self.callable_object = callable_object
+	
+	def __repr__(self):
+		return f"CallableToModuleWrapper({self.callable_object})"
 
 	def forward(self, *args, **kwargs):
 		return self.callable_object(*args, **kwargs)

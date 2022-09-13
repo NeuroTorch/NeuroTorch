68f030b25ae1297d5fc208cd1a732e9b1d77a525
Jérémie Gince <50332514+JeremieGince@users.noreply.github.com>
2022-09-13 11:25:41 -0400

    Update spikes_decoders.py

--- src/neurotorch/transforms/spikes_decoders.py
+++ src/neurotorch/transforms/spikes_decoders.py
@@ -1,3 +1,6 @@
+from typing import Union
+
+import numpy as np
 import torch
 
 from .base import to_tensor
@@ -7,7 +10,7 @@ class MeanConv(torch.nn.Module):
 	def __init__(
 			self,
 			kernel_size: int,
-			alpha: float = 1.0,
+			alpha: Union[float, torch.Tensor, np.ndarray] = 1.0,
 			learn_alpha: bool = True,
 			learn_kernel: bool = True,
 			activation: torch.nn.Module = torch.nn.Identity(),
@@ -16,12 +19,21 @@ class MeanConv(torch.nn.Module):
 		self.kernel_size = kernel_size
 		self.learn_kernel = learn_kernel
 		self.kernel = torch.nn.Parameter(torch.ones(1, self.kernel_size, 1), requires_grad=learn_kernel)
-		self.alpha = alpha
 		self.learn_alpha = learn_alpha
-		if learn_alpha:
-			self.alpha = torch.nn.Parameter(to_tensor(self.alpha))
+		self.alpha = torch.nn.Parameter(to_tensor(alpha), requires_grad=learn_alpha)
 		self.activation = activation
 	
+	def extra_repr(self):
+		extra_repr = f"kernel(size={self.kernel_size}, learn={self.learn_kernel})"
+		if self.learn_alpha:
+			extra_repr += f"\nalpha(size={tuple(self.alpha.shape)} learn={self.learn_alpha})"
+		else:
+			if torch.numel(self.alpha) == 1:
+				extra_repr += f"\nalpha(value={self.alpha.detach().cpu().item()})"
+			else:
+				extra_repr += f"\nalpha(size={tuple(self.alpha.shape)})"
+		return extra_repr
+		
 	def forward(self, inputs: torch.Tensor):
 		batch_size, time_steps, n_units = inputs.shape
 		inputs_view = torch.reshape(inputs, (batch_size, -1, self.kernel_size, n_units))

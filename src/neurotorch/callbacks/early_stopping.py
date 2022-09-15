4f8f7adbbdb92f4c13096d6855f31df78b47800e
Jérémie Gince <50332514+JeremieGince@users.noreply.github.com>
2022-09-14 23:20:37 -0400

    Update early_stopping.py

--- src/neurotorch/callbacks/early_stopping.py
+++ src/neurotorch/callbacks/early_stopping.py
@@ -23,4 +23,23 @@ class EarlyStopping(BaseCallback):
 		return np.all(np.abs(np.diff(losses)) < tol)
 		
 
-
+class EarlyStoppingThreshold(BaseCallback):
+	def __init__(
+			self,
+			*,
+			metric: str,
+			threshold: float,
+			minimize_metric: bool,
+	):
+		super().__init__()
+		self.threshold = threshold
+		self.metric = metric
+		self.minimize_metric = minimize_metric
+	
+	def on_iteration_end(self, trainer):
+		if self.minimize_metric:
+			threshold_met = trainer.training_history[self.metric][-1] < self.threshold
+		else:
+			threshold_met = trainer.training_history[self.metric][-1] > self.threshold
+		if threshold_met:
+			trainer.current_training_state = trainer.current_training_state.update(stop_training_flag=True)

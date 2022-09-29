383e7b95322c7aa235da4421df2d647b22ebb68f
Jérémie Gince <50332514+JeremieGince@users.noreply.github.com>
2022-09-28 22:25:04 -0400

    Update time_series_visualisation.py

--- src/neurotorch/visualisation/time_series_visualisation.py
+++ src/neurotorch/visualisation/time_series_visualisation.py
@@ -320,6 +320,7 @@ class Visualise:
 			n_spikes_steps: Optional[int] = None,
 			title: str = "",
 			desc: str = "Prediction",
+			**kwargs
 	) -> plt.Axes:
 		predictions, target = to_tensor(self._mean_given_timeseries[:, feature_index]), to_tensor(target)
 		if self.is_mean:
@@ -363,7 +364,7 @@ class Visualise:
 		ax.set_xlabel("Time [-]")
 		ax.set_ylabel("Activity [-]")
 		ax.set_title(title)
-		ax.legend()
+		ax.legend(loc=kwargs.get("legend_loc", "upper right"))
 		return ax
 	
 	def plot_timeseries_comparison(
@@ -394,6 +395,8 @@ class Visualise:
 		:param kwargs: Additional keyword arguments.
 		
 		:keyword bool close: Whether to close the figure after saving. Default: False.
+		:keyword int dpi: DPI of the figure. Default: 300.
+		:keyword str legend_loc: Location of the legend. Default: "upper right".
 		
 		:return: Figure and axes.
 		"""
@@ -439,22 +442,25 @@ class Visualise:
 			best_idx, axes[1], target[best_idx], best_spikes,
 			n_spikes_steps=n_spikes_steps,
 			title=f"Best {desc}", desc=desc,
+			**kwargs
 		)
 		self.plot_single_timeseries_comparison(
 			most_var_idx, axes[2], target[most_var_idx], most_var_spikes,
 			n_spikes_steps=n_spikes_steps,
 			title=f"Most Var {desc}", desc=desc,
+			**kwargs
 		)
 		self.plot_single_timeseries_comparison(
 			worst_idx, axes[3], target[worst_idx], worst_spikes,
 			n_spikes_steps=n_spikes_steps,
 			title=f"Worst {desc}", desc=desc,
+			**kwargs
 		)
 		
 		fig.set_tight_layout(True)
 		if filename is not None:
 			os.makedirs(os.path.dirname(filename), exist_ok=True)
-			fig.savefig(filename)
+			fig.savefig(filename, dpi=kwargs.get("dpi", 300))
 		if show:
 			plt.show()
 		if kwargs.get("close", False):

e1dc7f1a30e27aa1979aa17e0d72dadeab116656
Anthony Drouin <93488840+AnthoDrouin@users.noreply.github.com>
2022-09-30 22:13:18 -0400

    update tutorial

--- tutorials/time_series_forecasting_wilson_cowan/dataset.py
+++ tutorials/time_series_forecasting_wilson_cowan/dataset.py
@@ -1,5 +1,7 @@
 import torch
 from torch.utils.data import Dataset
+import numpy as np
+from scipy.ndimage import gaussian_filter1d
 
 
 class WSDataset(Dataset):
@@ -8,11 +10,29 @@ class WSDataset(Dataset):
 	This dataset is usefull to reproduce a time series using Wilson-Cowan layers.
 	"""
 
-	def __init__(self, x):
+	def __init__(
+			self,
+			path: str,
+			sample_size: int = 200,
+			smoothing_sigma: float = 10.0,
+			device: torch.device = torch.device("cpu"),
+	):
 		"""
-		:param x: Time series of shape (num_step, num_neurons)
+		:param path: path to the time series file
+		:param sample_size: number of neuron to use for training
+		:param smoothing_sigma: sigma for the gaussian smoothing
 		"""
-		self.x = x
+		ts = np.load(path)
+		n_neurons, n_shape = ts.shape
+		sample = np.random.randint(n_neurons, size=sample_size)
+		data = ts[sample, :]
+
+		for neuron in range(data.shape[0]):
+			data[neuron, :] = gaussian_filter1d(data[neuron, :], sigma=smoothing_sigma)
+			data[neuron, :] = data[neuron, :] - np.min(data[neuron, :])
+			data[neuron, :] = data[neuron, :] / np.max(data[neuron, :])
+		self.original_time_series = data
+		self.x = torch.tensor(data.T, dtype=torch.float32, device=device)
 
 	def __len__(self):
 		"""
@@ -26,3 +46,11 @@ class WSDataset(Dataset):
 		return the initial condition and the time series that will be use for training.
 		"""
 		return torch.unsqueeze(self.x[0], dim=0), self.x[1:]
+
+	@property
+	def get_full_time_series(self):
+		return self.x[None, :, :]
+
+	@property
+	def get_original_time_series(self):
+		return self.original_time_series

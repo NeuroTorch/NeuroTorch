0284d63431f2dcf7c1a040241b01d8f9b278c19e
Jérémie Gince <50332514+JeremieGince@users.noreply.github.com>
2022-10-28 15:12:50 -0400

    Update tbptt.py

--- src/neurotorch/learning_algorithms/tbptt.py
+++ src/neurotorch/learning_algorithms/tbptt.py
@@ -24,11 +24,11 @@ class TBPTT(BPTT):
 		self._auto_set_backward_time_steps = backward_time_steps is None
 		self.backward_time_steps = backward_time_steps
 		self._auto_backward_time_steps_ratio = kwargs.get("auto_backward_time_steps_ratio", 0.1)
-		assert 0 < self._auto_backward_time_steps_ratio < 1, "auto_backward_time_steps_ratio must be between 0 and 1"
+		assert 0 <= self._auto_backward_time_steps_ratio <= 1, "auto_backward_time_steps_ratio must be between 0 and 1"
 		self._auto_set_optim_time_steps = optim_time_steps is None
 		self.optim_time_steps = optim_time_steps
 		self._auto_optim_time_steps_ratio = kwargs.get("auto_optim_time_steps_ratio", self._auto_backward_time_steps_ratio)
-		assert 0 < self._auto_optim_time_steps_ratio < 1, "auto_optim_time_steps_ratio must be between 0 and 1"
+		assert 0 <= self._auto_optim_time_steps_ratio <= 1, "auto_optim_time_steps_ratio must be between 0 and 1"
 		self._data_n_time_steps = 0
 		self._layers_buffer = defaultdict(list)
 		self._forwards_decorated = False
@@ -77,9 +77,9 @@ class TBPTT(BPTT):
 	
 	def _maybe_update_time_steps(self):
 		if self._auto_set_backward_time_steps:
-			self.backward_time_steps = int(self._auto_backward_time_steps_ratio * self._data_n_time_steps)
+			self.backward_time_steps = max(1, int(self._auto_backward_time_steps_ratio * self._data_n_time_steps))
 		if self._auto_set_optim_time_steps:
-			self.optim_time_steps = int(self._auto_optim_time_steps_ratio * self._data_n_time_steps)
+			self.optim_time_steps = max(1, int(self._auto_optim_time_steps_ratio * self._data_n_time_steps))
 		if self.backward_time_steps != self.optim_time_steps:
 			raise NotImplementedError("backward_time_steps != optim_time_steps is not implemented yet")
 	

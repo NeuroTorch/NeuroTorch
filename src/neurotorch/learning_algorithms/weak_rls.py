abfe488c72bd4e8d1b43173d73a5cdf349bf022d
Jérémie Gince <50332514+JeremieGince@users.noreply.github.com>
2022-10-28 15:12:52 -0400

    Update weak_rls.py

--- src/neurotorch/learning_algorithms/weak_rls.py
+++ src/neurotorch/learning_algorithms/weak_rls.py
@@ -1,14 +1,16 @@
-from typing import Optional, Sequence, Union, Dict, Callable, List
+from collections import defaultdict
+from typing import Optional, Sequence, Union, Dict, Callable, List, Tuple
 
 import numpy as np
 import torch
 
 from .learning_algorithm import LearningAlgorithm
+from ..learning_algorithms.tbptt import TBPTT
 from ..transforms.base import ToDevice
-from ..utils import compute_jacobian
+from ..utils import compute_jacobian, list_insert_replace_at
 
 
-class WeakRLS(LearningAlgorithm):
+class WeakRLS(TBPTT):
 	r"""
 	Apply the backpropagation through time algorithm to the given model.
 	"""
@@ -20,8 +22,11 @@ class WeakRLS(LearningAlgorithm):
 			params: Optional[Sequence[torch.nn.Parameter]] = None,
 			layers: Optional[Union[Sequence[torch.nn.Module], torch.nn.Module]] = None,
 			criterion: Optional[Union[Dict[str, Union[torch.nn.Module, Callable]], torch.nn.Module, Callable]] = None,
+			backward_time_steps: Optional[int] = None,
+			is_recurrent: bool = False,
 			**kwargs
 	):
+		kwargs.setdefault("auto_backward_time_steps_ratio", 0)
 		"""
 		Constructor for WeakRLS class.
 
@@ -36,7 +41,15 @@ class WeakRLS(LearningAlgorithm):
 		"""
 		kwargs.setdefault("save_state", True)
 		kwargs.setdefault("load_state", True)
-		super().__init__(**kwargs)
+		super().__init__(
+			params=params,
+			layers=layers,
+			criterion=criterion,
+			backward_time_steps=backward_time_steps,
+			optimizer=None,
+			optim_time_steps=None,
+			**kwargs
+		)
 		if params is None:
 			params = []
 		else:
@@ -53,7 +66,7 @@ class WeakRLS(LearningAlgorithm):
 		# RLS attributes
 		self.K = None
 		self.P = None
-		self.delta = kwargs.get("delta", 1000.0)
+		self.delta = kwargs.get("delta", 1.0)
 		self.lambda_0 = kwargs.get("lambda_0", 0.99)
 		self.Lambda = kwargs.get("Lambda", 0.94)
 		self.a = kwargs.get("a", 1e-2)
@@ -64,12 +77,16 @@ class WeakRLS(LearningAlgorithm):
 		self.to_cpu_transform = ToDevice(device=torch.device("cpu"))
 		self.to_device_transform = None
 		self.reduction = kwargs.get("reduction", "mean").lower()
-		self._consider_other_dims_as_batch = kwargs.get("consider_other_dims_as_batch", False)
+		self._other_dims_as_batch = kwargs.get("other_dims_as_batch", False)
+		self._is_recurrent = is_recurrent
+		self.jacobian_noise = kwargs.get("jacobian_noise", 0.0)
+		
 		self._asserts()
+		self.t_list = []
 	
 	@property
 	def eta(self):
-		return self.alpha * (1 - self.alpha)
+		return max(1e-2, self.alpha * (1 - self.alpha))
 	
 	def _asserts(self):
 		assert self.a < self.b, "a must be less than b"
@@ -81,13 +98,13 @@ class WeakRLS(LearningAlgorithm):
 	
 	def _initialize_K(self, m):
 		self.K = [
-			torch.zeros((param.numel(), m), dtype=torch.float32, device=torch.device("cpu"))
+			torch.zeros((param.numel(), m), dtype=torch.float32, device=torch.device("cpu")).T
 			for param in self.params
 		]
 	
-	def _initialize_P(self):
+	def _initialize_P(self, m=None):
 		self.P = [
-			self.delta * torch.eye(param.numel(), dtype=torch.float32, device=torch.device("cpu"))
+			self.delta * torch.eye(param.numel() if m is None else m, dtype=torch.float32, device=torch.device("cpu"))
 			for param in self.params
 		]
 	
@@ -124,7 +141,7 @@ class WeakRLS(LearningAlgorithm):
 	
 	def _update_p(self, psi):
 		eyes = [
-			self.to_device_transform(torch.eye(self.params[i].numel()))
+			self.to_device_transform(torch.eye(self.P[i].shape[0]))
 			for i in range(len(self.params))
 		]
 		self.P = [
@@ -142,8 +159,10 @@ class WeakRLS(LearningAlgorithm):
 			self.zero_grad()
 			outputs.backward(grad_outputs[i], retain_graph=True)
 			for p_idx, param in enumerate(self.params):
-				psi[p_idx].append(param.grad.view(-1).detach().clone())
-		psi = [torch.stack(psi[i], dim=-1) for i in range(len(list(self.params)))]
+				grad = param.grad.view(-1).detach().clone()
+				grad_noise = torch.randn_like(grad) * self.jacobian_noise
+				psi[p_idx].append(grad + grad_noise)
+		psi = [torch.stack(psi[i], dim=-1).T for i in range(len(list(self.params)))]
 		# psi = compute_jacobian(params=self.params, y=outputs, strategy="slow")
 		# psi = [param.grad.view(-1, 1).detach().clone() for param in self.params]
 		return psi
@@ -157,9 +176,39 @@ class WeakRLS(LearningAlgorithm):
 		psi = [torch.stack(psi[i], dim=0) for i in range(len(self.params))]
 		return psi
 	
-	def _update_theta(self):
+	def _update_params(self):
+		# for param, k in zip(self.params, self.K):
+		# 	param.data += (k.T @ self.Delta.view(-1, 1)).to(param.device, non_blocking=True).view(param.data.shape)
 		for param, k in zip(self.params, self.K):
-			param.data += (k @ self.Delta.view(-1, 1)).to(param.device, non_blocking=True).view(param.data.shape)
+			param.grad = -(k.T @ self.Delta.view(-1, 1)).to(param.device, non_blocking=True).view(param.data.shape)
+		self.optimizer.step()
+	
+	def _maybe_update_time_steps(self):
+		if self._auto_set_backward_time_steps:
+			self.backward_time_steps = max(1, int(self._auto_backward_time_steps_ratio * self._data_n_time_steps))
+	
+	def _decorate_forward(self, forward, layer_name: str):
+		def _forward(*args, **kwargs):
+			out = forward(*args, **kwargs)
+			t = kwargs.get("t", None)
+			self.t_list.append(t)
+			if t is None:
+				return out
+			out_tensor = self._get_out_tensor(out)
+			list_insert_replace_at(self._layers_buffer[layer_name], t % self.backward_time_steps, out_tensor)
+			if len(self._layers_buffer[layer_name]) == self.backward_time_steps and t != 0:
+				# self._backward_at_t(t, self.backward_time_steps, layer_name)
+				self._backward_at_t(t, len(self._layers_buffer[layer_name]), layer_name)
+				out = self._detach_out(out)
+			return out
+		
+		return _forward
+	
+	def _backward_at_t(self, t: int, backward_t: int, layer_name: str):
+		y_batch = self._get_y_batch_slice_from_trainer((t + 1) - backward_t, t + 1, layer_name)
+		pred_batch = self._get_pred_batch_from_buffer(layer_name)
+		self._batch_step(pred_batch, y_batch)
+		self._layers_buffer[layer_name].clear()
 	
 	def load_checkpoint_state(self, trainer, checkpoint: dict, **kwargs):
 		if self.save_state:
@@ -173,9 +222,11 @@ class WeakRLS(LearningAlgorithm):
 		return None
 	
 	def start(self, trainer, **kwargs):
-		super().start(trainer)
+		LearningAlgorithm.start(self, trainer, **kwargs)
 		if not self.params:
 			self.params = list(trainer.model.parameters())
+		
+		self.optimizer = torch.optim.SGD(self.params, lr=1e-2)
 		
 		if self.criterion is None and trainer.criterion is not None:
 			self.criterion = trainer.criterion
@@ -183,7 +234,22 @@ class WeakRLS(LearningAlgorithm):
 		if self._device is None:
 			self._device = trainer.model.device
 		self.to_device_transform = ToDevice(device=self._device)
-		self._initialize_P()
+		# self._initialize_P()
+		self.output_layers: torch.nn.ModuleDict = trainer.model.output_layers
+		self._initialize_original_forwards()
+	
+	def on_batch_begin(self, trainer, **kwargs):
+		LearningAlgorithm.on_batch_begin(self, trainer, **kwargs)
+		self.trainer = trainer
+		if self._is_recurrent:
+			self._data_n_time_steps = self._get_data_time_steps_from_y_batch(trainer.current_training_state.y_batch)
+			self._maybe_update_time_steps()
+			self.decorate_forwards()
+	
+	def on_batch_end(self, trainer, **kwargs):
+		super().on_batch_end(trainer)
+		self.undecorate_forwards()
+		self._layers_buffer.clear()
 	
 	def apply_criterion(self, pred_batch, y_batch, criterion):
 		if criterion is None:
@@ -222,23 +288,24 @@ class WeakRLS(LearningAlgorithm):
 		self._update_k(psi)
 		self._update_p(psi)
 		
-	def _batch_step(self, trainer, **kwargs):
-		y_batch = trainer.current_training_state.y_batch
-		pred_batch = trainer.format_pred_batch(trainer.current_training_state.pred_batch, y_batch)
+	def _batch_step(self, pred_batch, y_batch):
+		model_device = self.trainer.model.device
 		assert isinstance(pred_batch, torch.Tensor), "pred_batch must be a torch.Tensor"
 		assert isinstance(y_batch, torch.Tensor), "y_batch must be a torch.Tensor"
 		
-		if self._consider_other_dims_as_batch:
+		if self._other_dims_as_batch:
 			pred_batch_view, y_batch_view = pred_batch.view(-1, pred_batch.shape[-1]), y_batch.view(-1, y_batch.shape[-1])
 		else:
 			pred_batch_view, y_batch_view = pred_batch.view(pred_batch.shape[0], -1), y_batch.view(y_batch.shape[0], -1)
 		self.zero_grad()
 		
-		error = self.to_device_transform(y_batch_view - pred_batch_view)
-		# mse_loss = torch.nn.MSELoss()(pred_batch_view, y_batch_view)
-		# mse_loss.backward()
-		# error = mse_loss.unsqueeze(0)
+		if self.K is None:
+			self._initialize_K(m=y_batch_view.shape[-1])
+		if self.P is None:
+			self._initialize_P(m=y_batch_view.shape[-1])
+		self._try_put_on_device(self.trainer)
 		
+		error = self.to_device_transform(y_batch_view - pred_batch_view)
 		if self.reduction == "mean":
 			error = error.mean(dim=0).unsqueeze(0)
 			psi = self._get_psi_batch(pred_batch_view.mean(dim=0).unsqueeze(0))
@@ -251,13 +318,20 @@ class WeakRLS(LearningAlgorithm):
 			raise ValueError(f"reduction must be one of 'mean', 'sum', 'none', got {self.reduction}")
 		
 		psi = self.to_device_transform(psi)
-		self._update_lambda()
+		self.psi = psi
+		
 		for idx, error_i in enumerate(error):
 			single_psi = [psi_i[idx] for psi_i in psi]
 			self._update_k_p_on_datum(psi=single_psi, error=error[idx])
-		self._update_alpha()
 		self._update_delta(error.mean(dim=0))
-		self._update_theta()
+		self._update_params()
+		self._put_on_cpu()
+		self.trainer.model.to(model_device, non_blocking=True)
+	
+	def on_iteration_begin(self, trainer, **kwargs):
+		# self._update_lambda()
+		# self._update_alpha()
+		pass
 	
 	def _try_put_on_device(self, trainer):
 		try:
@@ -278,16 +352,14 @@ class WeakRLS(LearningAlgorithm):
 	def on_optimization_begin(self, trainer, **kwargs):
 		y_batch = trainer.current_training_state.y_batch
 		pred_batch = trainer.format_pred_batch(trainer.current_training_state.pred_batch, y_batch)
-		model_device = trainer.model.device
 		
-		if self.K is None:
-			self._initialize_K(m=y_batch.shape[-1])
-			# self._initialize_K(m=1)
-		
-		self._try_put_on_device(trainer)
-		self._batch_step(trainer, **kwargs)
-		self._put_on_cpu()
-		trainer.model.to(model_device, non_blocking=True)
+		if self._is_recurrent:
+			for layer_name in self._layers_buffer:
+				backward_t = len(self._layers_buffer[layer_name])
+				if backward_t > 0:
+					self._backward_at_t(self._data_n_time_steps - 1, backward_t, layer_name)
+		else:
+			self._batch_step(trainer, **kwargs)
 		
 		trainer.update_state_(batch_loss=self.apply_criterion(pred_batch, y_batch, self.eval_criterion).detach_())
 	
@@ -295,6 +367,16 @@ class WeakRLS(LearningAlgorithm):
 		self.zero_grad()
 		# eval_loss = self.compute_eval_loss(trainer, **kwargs)
 		# trainer.update_itr_metrics_state_(eval_criterion=eval_loss)
+		metrics = dict(
+			psi_mean=self.to_cpu_transform(torch.cat([p.view(-1) for p in self.psi])).mean(),
+			K_mean=self.to_cpu_transform(torch.cat([k.view(-1) for k in self.K])).mean(),
+			P_mean=self.to_cpu_transform(torch.cat([p.view(-1) for p in self.P])).mean(),
+			Delta_mean=self.to_cpu_transform(self.Delta).mean(),
+			# alpha=self.alpha,
+			# eta=self.eta,
+			# lbda=self.Lambda,
+		)
+		trainer.update_itr_metrics_state_(**metrics)
 	
 	def compute_eval_loss(self, trainer, **kwargs):
 		y_batch = trainer.current_training_state.y_batch

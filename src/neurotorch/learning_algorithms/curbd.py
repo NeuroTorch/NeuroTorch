3eb1826136f87b5901e851d86edeacfcf8ffbe45
Jérémie Gince <50332514+JeremieGince@users.noreply.github.com>
2022-10-29 11:50:03 -0400

    Update curbd.py

--- src/neurotorch/learning_algorithms/curbd.py
+++ src/neurotorch/learning_algorithms/curbd.py
@@ -1,14 +1,16 @@
-from typing import Optional, Sequence, Union, Dict, Callable, List
+from collections import defaultdict
+from typing import Optional, Sequence, Union, Dict, Callable, List, Tuple
 
 import numpy as np
 import torch
 
 from .learning_algorithm import LearningAlgorithm
+from ..learning_algorithms.tbptt import TBPTT
 from ..transforms.base import ToDevice
-from ..utils import compute_jacobian
+from ..utils import compute_jacobian, list_insert_replace_at
 
 
-class CURBD(LearningAlgorithm):
+class CURBD(TBPTT):
 	r"""
 	Apply the backpropagation through time algorithm to the given model.
 	"""
@@ -20,8 +22,11 @@ class CURBD(LearningAlgorithm):
 			params: Optional[Sequence[torch.nn.Parameter]] = None,
 			layers: Optional[Union[Sequence[torch.nn.Module], torch.nn.Module]] = None,
 			criterion: Optional[Union[Dict[str, Union[torch.nn.Module, Callable]], torch.nn.Module, Callable]] = None,
+			backward_time_steps: Optional[int] = None,
+			is_recurrent: bool = False,
 			**kwargs
 	):
+		kwargs.setdefault("auto_backward_time_steps_ratio", 0)
 		"""
 		Constructor for WeakRLS class.
 
@@ -36,7 +41,15 @@ class CURBD(LearningAlgorithm):
 		"""
 		kwargs.setdefault("save_state", True)
 		kwargs.setdefault("load_state", True)
-		super().__init__(**kwargs)
+		super().__init__(
+			params=params,
+			layers=layers,
+			criterion=criterion,
+			backward_time_steps=backward_time_steps,
+			optimizer=None,
+			optim_time_steps=None,
+			**kwargs
+		)
 		if params is None:
 			params = []
 		else:
@@ -63,12 +76,17 @@ class CURBD(LearningAlgorithm):
 		self._device = kwargs.get("device", None)
 		self.to_cpu_transform = ToDevice(device=torch.device("cpu"))
 		self.to_device_transform = None
-		self.reduction = kwargs.get("reduction", "none").lower()
+		self.reduction = kwargs.get("reduction", "mean").lower()
+		self._other_dims_as_batch = kwargs.get("other_dims_as_batch", False)
+		self._is_recurrent = is_recurrent
+		self.jacobian_noise = kwargs.get("jacobian_noise", 0.0)
+		
 		self._asserts()
+		self.t_list = []
 	
 	@property
 	def eta(self):
-		return self.alpha * (1 - self.alpha)
+		return max(1e-2, self.alpha * (1 - self.alpha))
 	
 	def _asserts(self):
 		assert self.a < self.b, "a must be less than b"
@@ -80,13 +98,13 @@ class CURBD(LearningAlgorithm):
 	
 	def _initialize_K(self, m):
 		self.K = [
-			torch.zeros((param.numel(), m), dtype=torch.float32, device=torch.device("cpu"))
+			torch.zeros((param.numel(), m), dtype=torch.float32, device=torch.device("cpu")).T
 			for param in self.params
 		]
 	
-	def _initialize_P(self):
+	def _initialize_P(self, m=None):
 		self.P = [
-			self.delta * torch.eye(param.numel(), dtype=torch.float32, device=torch.device("cpu"))
+			self.delta * torch.eye(param.numel() if m is None else m, dtype=torch.float32, device=torch.device("cpu"))
 			for param in self.params
 		]
 	
@@ -96,31 +114,35 @@ class CURBD(LearningAlgorithm):
 	def _update_delta(self, error):
 		self.Delta = self.eta * error + self.alpha * self.Delta
 	
+	# self.Delta = 0.1 * error + 0.9 * self.Delta
+	# self.Delta = self.alpha * error + self.eta * self.Delta
+	
 	def _update_lambda(self):
 		self.Lambda = self.lambda_0 * self.Lambda + self.lambda_0 * (1 - self.lambda_0)
 	
 	def _update_k(self, psi):
-		P_psi_list = [
-			torch.einsum('ll,lm->lm', self.P[i], psi[i])
-			for i in range(len(self.params))
-		]
-		A_list = [
-			(self.Lambda + psi[i].T @ P_psi_list[i]).T
-			for i in range(len(self.params))
-		]
-		rank_list = [torch.linalg.matrix_rank(A) for A in A_list]
-		self.K = [
-			# P_psi_list[i] / (self.Lambda + psi[i].T @ P_psi_list[i])
-			# P_psi_list[i] / A_list[i]
-			# torch.linalg.solve((self.Lambda + psi[i].T @ P_psi_list[i]).T, P_psi_list[i].T).T
-			torch.linalg.lstsq(A_list[i], P_psi_list[i].T).solution.T
-			# P_psi_list[i] @ torch.linalg.inv(self.Lambda + psi[i].T @ P_psi_list[i])
-			for i in range(len(self.params))
-		]
+		# P_psi_list = [
+		# 	torch.einsum('ll,lm->lm', self.P[i], psi[i])
+		# 	for i in range(len(self.params))
+		# ]
+		# A_list = [
+		# 	(self.Lambda + psi[i].T @ P_psi_list[i]).T
+		# 	for i in range(len(self.params))
+		# ]
+		# rank_list = [torch.linalg.matrix_rank(A) for A in A_list]
+		# self.K = [
+		# 	# P_psi_list[i] / (self.Lambda + psi[i].T @ P_psi_list[i])
+		# 	# P_psi_list[i] / A_list[i]
+		# 	# torch.linalg.solve((self.Lambda + psi[i].T @ P_psi_list[i]).T, P_psi_list[i].T).T
+		# 	torch.linalg.lstsq(A_list[i], P_psi_list[i].T).solution.T
+		# 	# P_psi_list[i] @ torch.linalg.inv(self.Lambda + psi[i].T @ P_psi_list[i])
+		# 	for i in range(len(self.params))
+		# ]
+		self.K = [self.P[i] @ psi[i] for i in range(len(self.params))]
 	
 	def _update_p(self, psi):
 		eyes = [
-			self.to_device_transform(torch.eye(self.params[i].numel()))
+			self.to_device_transform(torch.eye(self.P[i].shape[0]))
 			for i in range(len(self.params))
 		]
 		self.P = [
@@ -129,15 +151,20 @@ class CURBD(LearningAlgorithm):
 		]
 	
 	def _get_psi(self, outputs):
+		# TODO: check https://medium.com/@monadsblog/pytorch-backward-function-e5e2b7e60140
+		# TODO: see https://pytorch.org/tutorials/beginner/blitz/autograd_tutorial.html#sphx-glr-beginner-blitz-autograd-tutorial-py
 		# return [self.to_device_transform(param.grad.view(-1)) for param in self.params]
-		# psi = [[] for _ in range(len(self.params))]
-		# for output in outputs:
-		# 	self.zero_grad()
-		# 	output.backward()
-		# 	for i, param in enumerate(self.params):
-		# 		psi[i].append(self.to_device_transform(param.grad.view(-1)))
-		# psi = [torch.stack(psi[i], dim=0) for i in range(len(self.params))]
-		psi = compute_jacobian(params=self.params, y=outputs, strategy="slow")
+		psi = [[] for _ in range(len(list(self.params)))]
+		grad_outputs = torch.eye(outputs.shape[-1])
+		for i in range(outputs.shape[-1]):
+			self.zero_grad()
+			outputs.backward(grad_outputs[i], retain_graph=True)
+			for p_idx, param in enumerate(self.params):
+				grad = param.grad.view(-1).detach().clone()
+				grad_noise = torch.randn_like(grad) * self.jacobian_noise
+				psi[p_idx].append(grad + grad_noise)
+		psi = [torch.stack(psi[i], dim=-1).T for i in range(len(list(self.params)))]
+		# psi = compute_jacobian(params=self.params, y=outputs, strategy="slow")
 		# psi = [param.grad.view(-1, 1).detach().clone() for param in self.params]
 		return psi
 	
@@ -150,11 +177,40 @@ class CURBD(LearningAlgorithm):
 		psi = [torch.stack(psi[i], dim=0) for i in range(len(self.params))]
 		return psi
 	
-	def _update_theta(self):
+	def _update_params(self):
 		for param, k in zip(self.params, self.K):
-			param.data += (self.Delta @ k.T).to(param.device, non_blocking=True).view(param.data.shape)
+			param.data += (k.T @ self.Delta.view(-1, 1)).to(param.device, non_blocking=True).view(param.data.shape)
 	
-	# param.data -= (k * self.Delta).to(param.device, non_blocking=True).view(param.data.shape)
+	# for param, k in zip(self.params, self.K):
+	# 	param.grad = -(k.T @ self.Delta.view(-1, 1)).to(param.device, non_blocking=True).view(param.data.shape)
+	# self.optimizer.step()
+	
+	def _maybe_update_time_steps(self):
+		if self._auto_set_backward_time_steps:
+			self.backward_time_steps = max(1, int(self._auto_backward_time_steps_ratio * self._data_n_time_steps))
+	
+	def _decorate_forward(self, forward, layer_name: str):
+		def _forward(*args, **kwargs):
+			out = forward(*args, **kwargs)
+			t = kwargs.get("t", None)
+			self.t_list.append(t)
+			if t is None:
+				return out
+			out_tensor = self._get_out_tensor(out)
+			list_insert_replace_at(self._layers_buffer[layer_name], t % self.backward_time_steps, out_tensor)
+			if len(self._layers_buffer[layer_name]) == self.backward_time_steps and t != 0:
+				# self._backward_at_t(t, self.backward_time_steps, layer_name)
+				self._backward_at_t(t, len(self._layers_buffer[layer_name]), layer_name)
+				out = self._detach_out(out)
+			return out
+		
+		return _forward
+	
+	def _backward_at_t(self, t: int, backward_t: int, layer_name: str):
+		y_batch = self._get_y_batch_slice_from_trainer((t + 1) - backward_t, t + 1, layer_name)
+		pred_batch = self._get_pred_batch_from_buffer(layer_name)
+		self._batch_step(pred_batch, y_batch)
+		self._layers_buffer[layer_name].clear()
 	
 	def load_checkpoint_state(self, trainer, checkpoint: dict, **kwargs):
 		if self.save_state:
@@ -168,9 +224,11 @@ class CURBD(LearningAlgorithm):
 		return None
 	
 	def start(self, trainer, **kwargs):
-		super().start(trainer)
+		LearningAlgorithm.start(self, trainer, **kwargs)
 		if not self.params:
 			self.params = list(trainer.model.parameters())
+		
+		self.optimizer = torch.optim.SGD(self.params, lr=1e-2)
 		
 		if self.criterion is None and trainer.criterion is not None:
 			self.criterion = trainer.criterion
@@ -178,7 +236,22 @@ class CURBD(LearningAlgorithm):
 		if self._device is None:
 			self._device = trainer.model.device
 		self.to_device_transform = ToDevice(device=self._device)
-		self._initialize_P()
+		# self._initialize_P()
+		self.output_layers: torch.nn.ModuleDict = trainer.model.output_layers
+		self._initialize_original_forwards()
+	
+	def on_batch_begin(self, trainer, **kwargs):
+		LearningAlgorithm.on_batch_begin(self, trainer, **kwargs)
+		self.trainer = trainer
+		if self._is_recurrent:
+			self._data_n_time_steps = self._get_data_time_steps_from_y_batch(trainer.current_training_state.y_batch)
+			self._maybe_update_time_steps()
+			self.decorate_forwards()
+	
+	def on_batch_end(self, trainer, **kwargs):
+		super().on_batch_end(trainer)
+		self.undecorate_forwards()
+		self._layers_buffer.clear()
 	
 	def apply_criterion(self, pred_batch, y_batch, criterion):
 		if criterion is None:
@@ -214,30 +287,42 @@ class CURBD(LearningAlgorithm):
 				param.grad.zero_()
 	
 	def _update_k_p_on_datum(self, psi, error):
-		self._update_p(psi)
 		self._update_k(psi)
+		self._update_p(psi)
 	
-	def _batch_step(self, trainer, **kwargs):
-		y_batch = trainer.current_training_state.y_batch
-		pred_batch = trainer.format_pred_batch(trainer.current_training_state.pred_batch, y_batch)
+	def _batch_step(self, pred_batch, y_batch):
+		model_device = self.trainer.model.device
 		assert isinstance(pred_batch, torch.Tensor), "pred_batch must be a torch.Tensor"
 		assert isinstance(y_batch, torch.Tensor), "y_batch must be a torch.Tensor"
 		
-		# pred_batch_view, y_batch_view = pred_batch.view(-1, pred_batch.shape[-1]), y_batch.view(-1, y_batch.shape[-1])
-		pred_batch_view, y_batch_view = pred_batch.view(pred_batch.shape[0], -1), y_batch.view(y_batch.shape[0], -1)
+		if self._other_dims_as_batch:
+			pred_batch_view, y_batch_view = pred_batch.view(-1, pred_batch.shape[-1]), y_batch.view(-1, y_batch.shape[-1])
+		else:
+			pred_batch_view, y_batch_view = pred_batch.view(pred_batch.shape[0], -1), y_batch.view(y_batch.shape[0], -1)
 		self.zero_grad()
 		
+		if self.K is None:
+			self._initialize_K(m=y_batch_view.shape[-1])
+		if self.P is None:
+			self._initialize_P(m=y_batch_view.shape[-1])
+		self._try_put_on_device(self.trainer)
+		
 		error = self.to_device_transform(y_batch_view - pred_batch_view)
-		# mse_loss = torch.nn.MSELoss()(pred_batch_view, y_batch_view)
-		# mse_loss.backward()
-		# error = mse_loss.unsqueeze(0)
 		
 		K = [pred_batch_view @ self.P[i] for i in range(len(self.params))]  # (B, m) @ (m, m) -> (B, m)
-		phiPphi = [K[i] @ pred_batch_view.T for i in range(len(self.params))]  # (B, m) @ (m, B) -> (B, B)
+		phiPphi = [(K[i] @ pred_batch_view.T).item() for i in range(len(self.params))]  # (B, m) @ (m, B) -> (B, B)
 		c = [1 / (1 + phiPphi[i]) for i in range(len(self.params))]  # (B, B)
 		self.P = [self.P[i] - c[i] * K[i] @ K[i].T for i in range(len(self.params))]  # (m, m) - (B, m) @ (m, B) -> (m, m)?
-		for param, k in zip(self.params, K):
-			param.data -= (c * error @ k.T).to(param.device, non_blocking=True).view(param.data.shape)
+		for i, (param, k) in enumerate(zip(self.params, K)):
+			param.data -= (c[i] * error.T @ k).to(param.device, non_blocking=True).view(param.data.shape)
+		
+		self._put_on_cpu()
+		self.trainer.model.to(model_device, non_blocking=True)
+	
+	def on_iteration_begin(self, trainer, **kwargs):
+		# self._update_lambda()
+		# self._update_alpha()
+		pass
 	
 	def _try_put_on_device(self, trainer):
 		try:
@@ -258,24 +343,32 @@ class CURBD(LearningAlgorithm):
 	def on_optimization_begin(self, trainer, **kwargs):
 		y_batch = trainer.current_training_state.y_batch
 		pred_batch = trainer.format_pred_batch(trainer.current_training_state.pred_batch, y_batch)
-		model_device = trainer.model.device
 		
-		if self.K is None:
-			self._initialize_K(m=y_batch.shape[-1])
-		# self._initialize_K(m=1)
-		
-		self._try_put_on_device(trainer)
-		self._batch_step(trainer, **kwargs)
-		self._put_on_cpu()
-		trainer.model.to(model_device, non_blocking=True)
+		if self._is_recurrent:
+			for layer_name in self._layers_buffer:
+				backward_t = len(self._layers_buffer[layer_name])
+				if backward_t > 0:
+					self._backward_at_t(self._data_n_time_steps - 1, backward_t, layer_name)
+		else:
+			self._batch_step(trainer, **kwargs)
 		
 		trainer.update_state_(batch_loss=self.apply_criterion(pred_batch, y_batch, self.eval_criterion).detach_())
 	
 	def on_optimization_end(self, trainer, **kwargs):
 		self.zero_grad()
-	
-	# eval_loss = self.compute_eval_loss(trainer, **kwargs)
-	# trainer.update_itr_metrics_state_(eval_criterion=eval_loss)
+		# eval_loss = self.compute_eval_loss(trainer, **kwargs)
+		# trainer.update_itr_metrics_state_(eval_criterion=eval_loss)
+		metrics = dict(
+			# psi_mean=self.to_cpu_transform(torch.cat([p.view(-1) for p in self.psi])).mean(),
+			# K_mean=self.to_cpu_transform(torch.cat([k.view(-1) for k in self.K])).mean(),
+			P_mean=self.to_cpu_transform(torch.cat([p.view(-1) for p in self.P])).mean(),
+			P_std=self.to_cpu_transform(torch.cat([p.view(-1) for p in self.P])).std(),
+			# Delta_mean=self.to_cpu_transform(self.Delta).mean(),
+			# alpha=self.alpha,
+			# eta=self.eta,
+			# lbda=self.Lambda,
+		)
+		trainer.update_itr_metrics_state_(**metrics)
 	
 	def compute_eval_loss(self, trainer, **kwargs):
 		y_batch = trainer.current_training_state.y_batch

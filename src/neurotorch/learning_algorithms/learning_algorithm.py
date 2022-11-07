9ebe07087fcf1882efa01bf9c2e677b91ac51c42
Jérémie Gince <50332514+JeremieGince@users.noreply.github.com>
2022-11-06 23:24:17 -0500

    Update learning_algorithm.py

--- src/neurotorch/learning_algorithms/learning_algorithm.py
+++ src/neurotorch/learning_algorithms/learning_algorithm.py
@@ -1,8 +1,31 @@
+from typing import List, Optional, Sequence
+
+import torch
+
 from ..callbacks.base_callback import BaseCallback
 
 
 class LearningAlgorithm(BaseCallback):
 	DEFAULT_PRIORITY = BaseCallback.DEFAULT_MEDIUM_PRIORITY
+	
+	def __init__(
+			self,
+			*,
+			params: Optional[Sequence[torch.nn.Parameter]] = None,
+			**kwargs
+	):
+		"""
+		Constructor for LearningAlgorithm class.
+		
+		:param params: The parameters to optimize. If None, the parameters of the model's trainer will be used.
+		:type params: Optional[Sequence[torch.nn.Parameter]]
+		"""
+		super().__init__(**kwargs)
+		if params is None:
+			params = []
+		else:
+			params = list(params)
+		self.params: List[torch.nn.Parameter] = params
 
 
 

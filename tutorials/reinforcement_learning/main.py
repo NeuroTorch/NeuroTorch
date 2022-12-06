ab1612c5178d5f9b09a7e6ce2ec5cf578c9f5271
Jérémie Gince <50332514+JeremieGince@users.noreply.github.com>
2022-12-05 19:27:57 -0500

    Update main.py

--- tutorials/reinforcement_learning/main.py
+++ tutorials/reinforcement_learning/main.py
@@ -3,41 +3,45 @@ This tutorial is a work in progress. It will be completed in the version 0.0.1 o
 """
 
 import gym
+import neurotorch as nt
 from neurotorch.rl.agent import Agent
 from neurotorch.rl.rl_academy import RLAcademy
 
 if __name__ == '__main__':
-    debug_space = gym.spaces.Dict({
-       "discrete": gym.spaces.Discrete(2),
-       "box": gym.spaces.Box(low=0, high=1, shape=(2,)),
-       "continuous": gym.spaces.Box(low=0, high=1, shape=(10,)),
-    })
     env_id = "LunarLander-v2"
-    env = gym.vector.make(env_id, num_envs=4, render_mode="human")
+    env = gym.vector.make(env_id, num_envs=6, render_mode="human")
+    checkpoint_manager = nt.CheckpointManager(
+        checkpoint_folder=f"data/tr_data/checkpoints_{env_id}_default-policy",
+        save_freq=10,
+        metric="rewards",
+        minimise_metric=False,
+        save_best_only=True,
+    )
     
     agent = Agent(
         env=env,
         behavior_name=env_id,
         policy=None,
+        policy_kwargs=dict(checkpoint_folder=checkpoint_manager.checkpoint_folder),
     )
     print(agent)
     
     academy = RLAcademy(
         agent=agent,
+        callbacks=[checkpoint_manager],
     )
-    history = academy.train(env, 10, n_epochs=3, batch_size=8, verbose=True)
+    history = academy.train(
+        env,
+        n_iterations=100,
+        n_epochs=3,
+        batch_size=1024,
+        load_checkpoint_mode=nt.LoadCheckpointMode.LAST_ITR,
+        verbose=True,
+    )
     history.plot(show=True)
     
     buffer, cumulative_rewards = academy.generate_trajectories(10, epsilon=0.0, verbose=True, env=env)
     print(f"Buffer: {buffer}")
     n_terminated = sum([int(e.terminal) for e in buffer])
     print(f"{n_terminated = }")
-    
-    # observation, info = env.reset(seed=42)
-    # for _ in range(1000):
-    #    action = agent.get_actions([observation])[0]
-    #    observation, reward, terminated, truncated, info = env.step(action)
-    #
-    #    if terminated or truncated:
-    #       observation, info = env.reset()
     env.close()

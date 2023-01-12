a5a009e3a192721a641a80e67b1145b508e8be04
Jérémie Gince <50332514+JeremieGince@users.noreply.github.com>
2023-01-12 11:42:21 -0500

    Update __init__.py

--- src/neurotorch/rl/__init__.py
+++ src/neurotorch/rl/__init__.py
@@ -1,8 +1,5 @@
 
 
-# raise NotImplementedError("This subpackage is not yet implemented. Will be implemented soon, stay tuned.")
-
-
 from .agent import Agent
 from .buffers import Experience, Trajectory, ReplayBuffer
 from .ppo import PPO
